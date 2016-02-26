31ff56d08ba97a94c6937cf857fd050fcec82084
Xi Lin <xilin@nlp.nlp.cs.washington.edu>
2016-02-25 22:14:37 -0800

    minor fixes

--- baseline1/giza++.sh
+++ baseline1/giza++.sh
@@ -4,9 +4,9 @@ INPUTDIR=$1
 SOURCE=$2
 TARGET=$3
 PREFIX=$4
-OUTPUTDIR=5
+OUTPUTDIR=$5
 
-./plain2snt.out ${INPUTDIR}${SOURCE} ${INPUTDIR}{TARGET}
+./plain2snt.out ${INPUTDIR}${SOURCE} ${INPUTDIR}${TARGET}
 
 MKCLSV2DIR="/home/xilin/Toolkits/giza-pp/mkcls-v2"
 cd ${MKCLSV2DIR}
@@ -19,5 +19,7 @@ GIZAPPDIR="/home/xilin/Toolkits/giza-pp/GIZA++-v2"
 cd ${GIZAPPDIR}
 
 # generate concurrence file
-./snt2cooc.out ${INPUTDIR}${SOURCE}.vcb ${INPUTDIR}${TARGET}.vcb ${INPUTDIR}${SOURCE}_${TARGET}.snt > ${INPUTDIR}{PREFIX}.cooc
-./GIZA++ -S ${INPUTDIR}${SOURCE}.vcb -T ${INPUTDIR}${TARGET}.vcb -C ${INPUTDIR}${SOURCE}_${TARGET}.snt -o ${PREFIX} -outputpath ${OUTPUTDIR}
+./snt2cooc.out ${INPUTDIR}${SOURCE}.vcb ${INPUTDIR}${TARGET}.vcb ${INPUTDIR}${SOURCE}_${TARGET}.snt > ${INPUTDIR}${SOURCE}_${TARGET}.cooc
+./GIZA++ -S ${INPUTDIR}${SOURCE}.vcb -T ${INPUTDIR}${TARGET}.vcb -C ${INPUTDIR}${SOURCE}_${TARGET}.snt -o ${SOURCE}_${TARGET} -outputpath ${OUTPUTDIR} -CoocurrenceFile ${INPUTDIR}${SOURCE}_${TARGET}.cooc
+./snt2cooc.out ${INPUTDIR}${TARGET}.vcb ${INPUTDIR}${SOURCE}.vcb ${INPUTDIR}${TARGET}_${SOURCE}.snt > ${INPUTDIR}${TARGET}_${SOURCE}.cooc
+./GIZA++ -S ${INPUTDIR}${TARGET}.vcb -T ${INPUTDIR}${SOURCE}.vcb -C ${INPUTDIR}${TARGET}_${SOURCE}.snt -o ${TARGET}_${SOURCE} -outputpath ${OUTPUTDIR} -CoocurrenceFile ${INPUTDIR}${TARGET}_${SOURCE}.cooc
