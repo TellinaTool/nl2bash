7d0e92a3b70e17417a55776a170a52a61615a94a
Calvin Loncaric <loncaric@cs.washington.edu>
2016-03-01 18:24:05 -0800

    Various improvements and refactoring in bash.py
    In particular:
    
        'xargs -n1' is now ['xargs', '-n', '1']
        'xargs -I {} find {} -type d' is now ['xargs', '-I', '{}'], ['find', '{}', '-type', 'd']

--- baseline1/src/bash.py
+++ baseline1/src/bash.py
@@ -5,6 +5,7 @@ Functions for parsing bash code. See `parse(code)`
 # builtin
 from __future__ import print_function
 import sys
+import re
 
 # 3rd party
 import bashlex
@@ -12,18 +13,43 @@ import bashlex.ast
 
 DEBUG = False
 
-def split_flags(word):
-    if word.startswith("--"):
-        for flag in word.split('='):
-            yield flag
-    elif word.startswith("-"):
-        """
-        turns -xyz into -x, -y, -z
-        """
-        for flag in word[1:]:
-            yield "-" + flag
+COMBINED_FLAG_AND_ARG = re.compile(r"^(\-\w)(\d)+$")
+def split_flags(cmd, word):
+    if cmd == "tar" and all(c in "xtcrujzvf" for c in word):
+        for c in word:
+            yield "-" + c
+    elif cmd != "find":
+        if word.startswith("--"):
+
+            for flag in word.split('='):
+                yield flag
+
+        elif word.startswith("-"):
+
+            # turn -n1 into -n, 1
+            match = COMBINED_FLAG_AND_ARG.match(word)
+            if match:
+                yield match.group(1)
+                yield match.group(2)
+
+            # turns -xyz into -x, -y, -z
+            else:
+                for flag in word[1:]:
+                    yield "-" + flag
+
+        else:
+            yield word
     else:
         yield word
+
+def test_split_flags(cmd, flag, expected):
+    print(" --> testing flag splitting on {} {}... ".format(cmd, flag), end="")
+    result = list(split_flags(cmd, flag))
+    if result == expected:
+        print("ok {}".format(result))
+    else:
+        print("got {}".format(result))
+        assert result == expected # False
 
 def parse(code):
     """
@@ -48,12 +74,12 @@ def parse(code):
                     args = []
                     for part in parts[1:]:
                         if part.kind == "word":
-                            if cmd == "tar" and all(c in "xtcrujzvf" for c in part.word):
-                                args += ["-" + c for c in part.word]
-                            elif cmd != "find":
-                                args += list(split_flags(part.word))
+                            if cmd == "xargs" and part.word in ["find", "mv", "cp", "tar", "rm"]:
+                                self.simplecmds.append([cmd] + args)
+                                args = []
+                                cmd = part.word
                             else:
-                                args.append(part.word)
+                                args += list(split_flags(cmd, part.word))
                     self.simplecmds.append([cmd] + args)
 
         v = Visitor()
@@ -64,5 +90,10 @@ def parse(code):
 if __name__ == "__main__":
     # This lets you call `python bash.py "cmd"` to test the parser
     DEBUG = True
+    test_split_flags("ls", "-lsh", ["-l", "-s", "-h"])
+    test_split_flags("tar", "--exclude=something", ["--exclude", "something"])
+    test_split_flags("tar", "czvf", ["-c", "-z", "-v", "-f"])
+    test_split_flags("find", "-iname", ["-iname"])
+    test_split_flags("xargs", "-n1", ["-n", "1"])
     for cmd in parse(" ".join(sys.argv[1:])):
         print(cmd)
