9b823ccb35a228e0421ff0c7142b552d0453ed49
Victoria Lin <xilin@cs.washington.edu>
2016-07-25 15:53:57 -0700

    merge

--- seq2seq/translate.py
+++ seq2seq/translate.py
@@ -323,7 +323,7 @@ def eval():
 
 def train_and_eval(train_set, dev_set):
     num_iter = 1000
-    for i in xrange(30):
+    for i in xrange(4):
         train(train_set, dev_set, num_iter)
         tf.reset_default_graph()
         eval()
