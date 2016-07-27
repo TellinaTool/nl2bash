a49d9441544755d7669225cea6455e83938f8a41
Victoria Lin <purenanya@gmail.com>
2016-07-27 16:35:59 -0700

    re-organize folders

--- ibm/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-MOSEDIR="/home/xilin/Toolkits/mosesdecoder"
-
-echo $1 | ${MOSEDIR}/bin/moses -f train/model/moses.ini 
--- tkmatch/src/common.py
+++
@@ -1,3 +0,0 @@
-
-def mangle_arg(root_cmd, arg_name):
-    return "{}:::{}".format(root_cmd, arg_name)
