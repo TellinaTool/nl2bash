42780f11b44f6aaa61515e70fa966f8d6bba6fe6
Victoria Lin <xilin@cs.washington.edu>
2016-07-27 15:47:43 -0700

    merge

--- seq2seq/seq2seq_model.py
+++ seq2seq/seq2seq_model.py
@@ -161,11 +161,12 @@ class Seq2SeqModel(object):
     if not forward_only:
       self.gradient_norms = []
       self.updates = []
-      opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate,
-                                                     beta1=0.9,
-                                                     beta2=0.999,
-                                                     epsilon=1e-08,
-                                                     use_locking=False, name='Adam')
+      opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+      # opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate,
+      #                                                beta1=0.9,
+      #                                                beta2=0.999,
+      #                                                epsilon=1e-08,
+      #                                                use_locking=False, name='Adam')
       for b in xrange(len(buckets)):
         gradients = tf.gradients(self.losses[b], params)
         clipped_gradients, norm = tf.clip_by_global_norm(gradients,
