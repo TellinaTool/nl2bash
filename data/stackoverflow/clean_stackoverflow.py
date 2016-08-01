9ca01f84855be5b7ed18f10f0d1d7b3bb83827eb
Victoria Lin <xilin@cs.washington.edu>
2016-07-31 23:38:09 -0700

    merge

--- data/stackoverflow/clean_stackoverflow.py
+++ data/stackoverflow/clean_stackoverflow.py
@@ -16,9 +16,9 @@ import sys
 sys.path.append("/home/xilin/Packages")
 from semantics.lexical import stanford_lemmatize
 
-sys.path.append("../baseline0/src")
 # local
 import bashlex.bash
+from data.utils import ENGLISH_STOPWORDS
 from make_model import PROMPT_REGEX
 from make_model import all_samples, is_oneliner
 
@@ -26,23 +26,7 @@ html = HTMLParser.HTMLParser()
 
 WORD_REGEX = re.compile(r"\w*-?\w+")
 # basic stop words list is from http://www.ranks.nl/stopwords/
-STOPWORDS = {"a", "an", "the",
-             "be", "'s", "been", "being", "was", "were", "here", "there", "do", "how",
-             "i", "i'd", "i'll", "i'm", "i've", "me", "my", "myself",
-             "can", "could", "did", "do", "does", "doing",
-             "must", "should", "would",
-             "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves",
-             "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his",
-             "she", "she'd", "she'll", "she's",
-             "it", "it's", "its", "itself",
-             "we", "we'd", "we'll", "we're", "we've",
-             "their", "theirs", "them", "themselves", "then", "there", "there's", "they", "they'd", "they'll", "they're", "they've",
-             "let", "let's",
-             "this", "that", "these", "those",
-             "what", "what's",
-             "which",
-             "how", "how's",
-             "but"}
+STOPWORDS = ENGLISH_STOPWORDS
 STOPWORDS |= { ",", ".", "!", "?", ";", ":", "\/", "\\/", "--"}
 STOPWORDS |= { "mac", "os", "x", "unix", "linux", "cmd", "bat", "bash", "commandline", "command-line", "shell", "script", "shell-script" }
 STOPWORDS -= { "not", "no" }
