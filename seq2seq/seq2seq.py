8de7a833c3dbcef623ef11e36697bccbf31daaaf
Victoria Lin <xilin@cs.washington.edu>
2016-08-02 11:05:03 -0700

    mf

--- seq2seq/seq2seq.py
+++ seq2seq/seq2seq.py
@@ -431,26 +431,162 @@ def embedding_tied_rnn_seq2seq(encoder_inputs, decoder_inputs, cell,
                                               lambda: decoder(False))
     return outputs_and_state[:-1], outputs_and_state[-1]
 
-
 def attention_decoder(decoder_inputs, initial_state, attention_states, cell,
                       output_size=None, num_heads=1, loop_function=None,
                       dtype=dtypes.float32, scope=None,
-                      initial_state_attention=False,
-                      beam_decoder=None):
+                      initial_state_attention=False):
   """RNN decoder with attention for the sequence-to-sequence model.
-
   In this context "attention" means that, during decoding, the RNN can look up
   information in the additional tensor attention_states, and it does this by
   focusing on a few entries from the tensor. This model has proven to yield
   especially good results in a number of sequence-to-sequence tasks. This
   implementation is based on http://arxiv.org/abs/1412.7449 (see below for
   details). It is recommended for complex sequence-to-sequence tasks.
+  Args:
+    decoder_inputs: A list of 2D Tensors [batch_size x input_size].
+    initial_state: 2D Tensor [batch_size x cell.state_size].
+    attention_states: 3D Tensor [batch_size x attn_length x attn_size].
+    cell: rnn_cell.RNNCell defining the cell function and size.
+    output_size: Size of the output vectors; if None, we use cell.output_size.
+    num_heads: Number of attention heads that read from attention_states.
+    loop_function: If not None, this function will be applied to i-th output
+      in order to generate i+1-th input, and decoder_inputs will be ignored,
+      except for the first element ("GO" symbol). This can be used for decoding,
+      but also for training to emulate http://arxiv.org/abs/1506.03099.
+      Signature -- loop_function(prev, i) = next
+        * prev is a 2D Tensor of shape [batch_size x output_size],
+        * i is an integer, the step number (when advanced control is needed),
+        * next is a 2D Tensor of shape [batch_size x input_size].
+    dtype: The dtype to use for the RNN initial state (default: tf.float32).
+    scope: VariableScope for the created subgraph; default: "attention_decoder".
+    initial_state_attention: If False (default), initial attentions are zero.
+      If True, initialize the attentions from the initial state and attention
+      states -- useful when we wish to resume decoding from a previously
+      stored decoder state and attention states.
+  Returns:
+    A tuple of the form (outputs, state), where:
+      outputs: A list of the same length as decoder_inputs of 2D Tensors of
+        shape [batch_size x output_size]. These represent the generated outputs.
+        Output i is computed from input i (which is either the i-th element
+        of decoder_inputs or loop_function(output {i-1}, i)) as follows.
+        First, we run the cell on a combination of the input and previous
+        attention masks:
+          cell_output, new_state = cell(linear(input, prev_attn), prev_state).
+        Then, we calculate new attention masks:
+          new_attn = softmax(V^T * tanh(W * attention_states + U * new_state))
+        and then we calculate the output:
+          output = linear(cell_output, new_attn).
+      state: The state of each decoder cell the final time-step.
+        It is a 2D Tensor of shape [batch_size x cell.state_size].
+  Raises:
+    ValueError: when num_heads is not positive, there are no inputs, shapes
+      of attention_states are not set, or input size cannot be inferred
+      from the input.
+  """
+  if not decoder_inputs:
+    raise ValueError("Must provide at least 1 input to attention decoder.")
+  if num_heads < 1:
+    raise ValueError("With less than 1 heads, use a non-attention decoder.")
+  if not attention_states.get_shape()[1:2].is_fully_defined():
+    raise ValueError("Shape[1] and [2] of attention_states must be known: %s"
+                     % attention_states.get_shape())
+  if output_size is None:
+    output_size = cell.output_size
+
+  with variable_scope.variable_scope(scope or "attention_decoder"):
+    batch_size = array_ops.shape(decoder_inputs[0])[0]  # Needed for reshaping.
+    attn_length = attention_states.get_shape()[1].value
+    attn_size = attention_states.get_shape()[2].value
+
+    # To calculate W1 * h_t we use a 1-by-1 convolution, need to reshape before.
+    hidden = array_ops.reshape(
+        attention_states, [-1, attn_length, 1, attn_size])
+    hidden_features = []
+    v = []
+    attention_vec_size = attn_size  # Size of query vectors for attention.
+    for a in xrange(num_heads):
+      k = variable_scope.get_variable("AttnW_%d" % a,
+                                      [1, 1, attn_size, attention_vec_size])
+      hidden_features.append(nn_ops.conv2d(hidden, k, [1, 1, 1, 1], "SAME"))
+      v.append(variable_scope.get_variable("AttnV_%d" % a,
+                                           [attention_vec_size]))
+
+    state = initial_state
+
+    def attention(query):
+      """Put attention masks on hidden using hidden_features and query."""
+      ds = []  # Results of attention reads will be stored here.
+      for a in xrange(num_heads):
+        with variable_scope.variable_scope("Attention_%d" % a):
+          y = linear(query, attention_vec_size, True)
+          y = array_ops.reshape(y, [-1, 1, 1, attention_vec_size])
+          # Attention mask is a softmax of v^T * tanh(...).
+          s = math_ops.reduce_sum(
+              v[a] * math_ops.tanh(hidden_features[a] + y), [2, 3])
+          a = nn_ops.softmax(s)
+          # Now calculate the attention-weighted vector d.
+          d = math_ops.reduce_sum(
+              array_ops.reshape(a, [-1, attn_length, 1, 1]) * hidden,
+              [1, 2])
+          ds.append(array_ops.reshape(d, [-1, attn_size]))
+      return ds
+
+    outputs = []
+    prev = None
+    batch_attn_size = array_ops.pack([batch_size, attn_size])
+    attns = [array_ops.zeros(batch_attn_size, dtype=dtype)
+             for _ in xrange(num_heads)]
+    for a in attns:  # Ensure the second shape of attention vectors is set.
+      a.set_shape([None, attn_size])
+    if initial_state_attention:
+      attns = attention(initial_state)
+    for i, inp in enumerate(decoder_inputs):
+      if i > 0:
+        variable_scope.get_variable_scope().reuse_variables()
+      # If loop_function is set, we use it instead of decoder_inputs.
+      if loop_function is not None and prev is not None:
+        with variable_scope.variable_scope("loop_function", reuse=True):
+          inp = loop_function(prev, i)
+      # Merge input and previous attentions into one vector of the right size.
+      input_size = inp.get_shape().with_rank(2)[1]
+      if input_size.value is None:
+        raise ValueError("Could not infer input size from input: %s" % inp.name)
+      x = linear([inp] + attns, input_size, True)
+      # Run the RNN.
+      cell_output, state = cell(x, state)
+      # Run the attention mechanism.
+      if i == 0 and initial_state_attention:
+        with variable_scope.variable_scope(variable_scope.get_variable_scope(),
+                                           reuse=True):
+          attns = attention(state)
+      else:
+        attns = attention(state)
+
+      with variable_scope.variable_scope("AttnOutputProjection"):
+        output = linear([cell_output] + attns, output_size, True)
+      if loop_function is not None:
+        prev = output
+      outputs.append(output)
+
+  return outputs, state
+
+def attention_beam_decoder(decoder_inputs, initial_state, attention_states, cell,
+                      beam_decoder, output_size=None, num_heads=1,
+                      loop_function=None, dtype=dtypes.float32, scope=None,
+                      initial_state_attention=False):
+  """RNN beam_search decoder with attention for the sequence-to-sequence model.
+
+  In this context "attention" means that, during decoding, the RNN can look up
+  information in the additional tensor attention_states, and it does this by
+  focusing on a few entries from the tensor. This function extends attention_decoder
+  to use beam search to generate top-K ranked results.
 
   Args:
     decoder_inputs: A list of 2D Tensors [batch_size x input_size].
     initial_state: 2D Tensor [batch_size x cell.state_size].
     attention_states: 3D Tensor [batch_size x attn_length x attn_size].
     cell: rnn_cell.RNNCell defining the cell function and size.
+    beam_decoder: beam-search decoder.
     output_size: Size of the output vectors; if None, we use cell.output_size.
     num_heads: Number of attention heads that read from attention_states.
     loop_function: If not None, this function will be applied to i-th output
@@ -490,10 +626,9 @@ def attention_decoder(decoder_inputs, initial_state, attention_states, cell,
       of attention_states are not set, or input size cannot be inferred
       from the input.
   """
-  if beam_decoder:
-    decoder_inputs = [beam_decoder.wrap_input(decoder_inputs)] + [None] * 4
-    attention_states = [beam_decoder.wrap_input(attention_states)] + [None] * 4
-    initial_state = beam_decoder.wrap_state(initial_state)
+  decoder_inputs = [beam_decoder.wrap_input(decoder_inputs)] + [None] * 4
+  attention_states = [beam_decoder.wrap_input(attention_states)] + [None] * 4
+  initial_state = beam_decoder.wrap_state(initial_state)
     
   if not decoder_inputs:
     raise ValueError("Must provide at least 1 input to attention decoder.")
@@ -652,11 +787,20 @@ def embedding_attention_decoder(decoder_inputs, initial_state, attention_states,
         update_embedding_for_previous) if feed_previous else None
     emb_inp = [
         embedding_ops.embedding_lookup(embedding, i) for i in decoder_inputs]
-    return attention_decoder(
-        emb_inp, initial_state, attention_states, cell, output_size=output_size,
+    if beam_decoder:
+      return attention_beam_decoder(
+        emb_inp, initial_state, attention_states, cell,
+        beam_decoder, output_size=output_size,
         num_heads=num_heads, loop_function=loop_function,
-        initial_state_attention=initial_state_attention,
-        beam_decoder=beam_decoder)
+        initial_state_attention=initial_state_attention
+      )
+    else:
+      return attention_decoder(
+        emb_inp, initial_state, attention_states, cell,
+        output_size=output_size,
+        num_heads=num_heads, loop_function=loop_function,
+        initial_state_attention=initial_state_attention
+      )
 
 
 def embedding_attention_seq2seq(encoder_inputs, decoder_inputs, cell,
