95be90281b90f447de821474750e3034877eb025
Victoria Lin <xilin@cs.washington.edu>
2016-08-02 00:10:18 -0700

    debug beam decoding with attention

--- seq2seq/translate.py
+++ seq2seq/translate.py
@@ -288,7 +288,7 @@ def decode(output_logits, rev_cm_vocab):
         # This is a greedy decoder - outputs are just argmaxes of output_logits.
         outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
     elif FLAGS.decoder == "beam_search":
-
+        outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
 
     # If there is an EOS symbol in outputs, cut them at that point.
     if data_utils.EOS_ID in outputs:
@@ -304,7 +304,8 @@ def batch_decode(output_logits, rev_cm_vocab):
         # This is a greedy decoder - outputs are just argmaxes of output_logits.
         predictions = [np.argmax(logit, axis=1) for logit in output_logits]
     elif FLAGS.decoder == "beam_search":
-
+        outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+        
     for i in xrange(batch_size):
         outputs = [int(pred[i]) for pred in predictions]
         # If there is an EOS symbol in outputs, cut them at that point.
