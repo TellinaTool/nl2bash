19c444d05d0792a5830d49150a210caf836408f9
Victoria Lin <xilin@cs.washington.edu>
2016-08-02 22:29:44 -0700

    add output_projection to attention cell wrapper

--- seq2seq/tf_beam_decoder.py
+++ seq2seq/tf_beam_decoder.py
@@ -16,6 +16,8 @@ best_sparse = beam_decoder.unwrap_output_sparse(final_state) # Output, this time
 
 import tensorflow as tf
 
+from tensorflow.python.ops import nn_ops
+
 try:
     from tensorflow.python.util import nest
 except ImportError:
@@ -164,7 +166,7 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
         self._nondone_mask = tf.reshape(tf.tile(self._nondone_mask, [1, self.beam_size, 1]),
             [-1, self.beam_size*self.num_classes])
 
-    def __call__(self, inputs, state, scope=None):
+    def __call__(self, inputs, state, scope=None, output_projection=None):
         (
             past_cand_symbols, # [batch_size, max_len]
             past_cand_logprobs,# [batch_size]
@@ -178,6 +180,9 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
 
         cell_inputs = inputs
         cell_outputs, raw_cell_state = self.cell(cell_inputs, past_cell_state)
+
+        if output_projection is not None:
+            cell_outputs = nn_ops.xw_plus_b(cell_outputs, output_projection[0], output_projection[1])
 
         logprobs = tf.nn.log_softmax(cell_outputs)
 
