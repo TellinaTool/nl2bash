4b3420d6f3de327f0719e92cf54ed67f93b61077
Victoria Lin <xilin@cs.washington.edu>
2016-08-15 12:00:17 -0700

    mf

--- seq2tree/parse_args.py
+++ seq2tree/parse_args.py
@@ -42,10 +42,6 @@ def define_input_flags():
     tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
     tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
                               "Learning rate decays by this much.")
-    tf.app.flags.DEFINE_float("input_keep_prob", 1.0,
-                              "Dropout: proportion of input units to keep.")
-    tf.app.flags.DEFINE_float("output_keep_prob", 1.0,
-                              "Dropout: proportion of output units to keep.")
     tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
                               "Clip gradients to this norm.")
     tf.app.flags.DEFINE_integer("batch_size", 64,
