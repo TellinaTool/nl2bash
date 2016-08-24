646a5f1207d2d0157ef9a1c2f37ca7253e527138
Victoria Lin <xilin@cs.washington.edu>
2016-08-24 16:38:01 -0700

    reorganize some files, use normalize_ast for command tokenization

--- bashlex/bash.py
+++ bashlex/bash.py
@@ -1,34 +1,14 @@
-"""
-Simplified bash tokenizer and parser built using bashlex.
-"""
-
 # builtin
 from __future__ import print_function
 
 import re
-import shlex
-import sys
-sys.path.append("../")
 
-from data.utils import ENGLISH_STOPWORDS, word2num
-
-import bast, errors, tokenizer, bparser
-
-from nltk.stem.wordnet import WordNetLemmatizer
-lmtzr = WordNetLemmatizer()
-
-DEBUG = False
-
-# Regular expressions used to tokenize an English sentence.
-# _WORD_SPLIT = re.compile(b"^\s+|\s*,\s*|\s+$|^[\(|\[|\{|\<|\'|\"|\`]|[\)|\]|\}|\>|\'|\"|\`]$")
-_WORD_SPLIT = re.compile(b"^\s+|\s*,\s*|\s+$|^[\(|\[|\{|\<]|[\)|\]|\}|\>]$")
-_WORD_SPLIT_RESPECT_QUOTES = re.compile(b'(?:[^\s,"]|"(?:\\.|[^"])*")+')
-_DIGIT_RE = re.compile(br"\d+")
+import bast, errors, tokenizer, bparser, utils
 
 _NUM = b"_NUM"
 _LONG_PATTERN = b"_LONG_PATTERN"
 
-COMBINED_FLAG_AND_ARG = re.compile(r"^(\-\w)(\d+)$")
+_DIGIT_RE = re.compile(br"\d+")
 
 head_commands = [
     "find", "xargs",
@@ -103,92 +83,7 @@ def rw_signature(cmd):
     signature = ' '.join(list(rws))
     return signature
 
-def basic_tokenizer(sentence, lower_case=True, normalize_digits=True,
-                    normalize_long_pattern=True,
-                    lemmatization=True):
-    """Very basic tokenizer: used for English tokenization."""
-    sentence = sentence.replace('`\'', '"') \
-            .replace('``', '"') \
-            .replace("''", '"') \
-            .replace(' \'', ' "') \
-            .replace('\' ', '" ') \
-            .replace('`', '"') \
-            .replace('(', '( ') \
-            .replace(')', ' )')
-            # .replace('[', '[ ') \
-            # .replace('{', '{ ') \
-            # .replace(']', ' ]') \
-            # .replace('}', ' }') \
-            # .replace('<', '< ') \
-            # .replace('>', ' >')
-    sentence = re.sub('^\'', '"', sentence)
-    sentence = re.sub('\'$', '"', sentence)
-
-    sentence = re.sub('(,\s+)|(,$)', ' ', sentence)
-    sentence = re.sub('(;\s+)|(;$)', ' ', sentence)
-    sentence = re.sub('(:\s+)|(:$)', ' ', sentence)
-    sentence = re.sub('(\.\s+)|(\.$)', ' ', sentence)
-
-    sentence = re.sub('\'s', '\\\'s', sentence)
-    sentence = re.sub('\'re', '\\\'re', sentence)
-    sentence = re.sub('\'ve', '\\\'ve', sentence)
-    sentence = re.sub('\'d', '\\\'d', sentence)
-    sentence = re.sub('\'t', '\\\'t', sentence)
-
-    words = re.findall(_WORD_SPLIT_RESPECT_QUOTES, sentence)
-
-    normalized_words = []
-    for i in xrange(len(words)):
-        word = words[i].strip()
-
-        # remove unnecessary upper cases
-        if lower_case:
-            if len(word) > 1 and word[0].isupper() and word[1:].islower():
-                word = word.lower()
-
-        # lemmatization
-        if lemmatization:
-            word = lmtzr.lemmatize(word)
-
-        # remove English stopwords
-        if word in ENGLISH_STOPWORDS:
-            continue
-
-        # covert number words into numbers
-        if word in word2num:
-            word = str(word2num[word])
-
-        # normalize regular expressions
-        if not is_english_word(word):
-            # msg = word + ' -> '
-            if not word.startswith('"'):
-                word = '"' + word
-            if not word.endswith('"'):
-                word = word + '"'
-            # msg += word
-            # print(msg)
-
-        # normalize long patterns
-        if ' ' in word and len(word) > 3:
-            try:
-                assert(word.startswith('"') and word.endswith('"'))
-            except AssertionError, e:
-                print("Quotation Error: space inside word " + sentence)
-            if normalize_long_pattern:
-                word = _LONG_PATTERN
-
-        # normalize digits
-        word = re.sub(_DIGIT_RE, _NUM, word) if normalize_digits and not is_option(word) else word
-
-        # convert possessive expression
-        if word.endswith("'s"):
-            normalized_words.append(word[:-2])
-            normalized_words.append("'s")
-        else:
-            normalized_words.append(word)
-
-    return normalized_words
-
+@utils.deprecated
 def bash_tokenizer(cmd, normalize_digits=True, normalize_long_pattern=True,
                    recover_quotation=True):
     cmd = cmd.replace('\n', ' ').strip()
@@ -339,108 +234,3 @@ def bash_tokenizer(cmd, normalize_digits=True, normalize_long_pattern=True,
 
     return [token.encode('utf-8') for token in tokens]
 
-def basic_tokenizer_regex(sentence, normalize_digits=True, lower_case=True):
-    """Very basic tokenizer: used for English tokenization."""
-    words = []
-    for space_separated_fragment in sentence.replace('\n', ' ').strip().split():
-        words.extend(re.split(_WORD_SPLIT, space_separated_fragment))
-    normalized_words = []
-    for i in xrange(len(words)):
-        w = words[i].strip()
-        word = re.sub(_DIGIT_RE, _NUM, w) if normalize_digits and not is_option(w) else w
-        if lower_case:
-            # remove unnecessary upper cases
-            if len(word) > 1 and word[0].isupper() and word[1:].islower():
-                word = word.lower()
-        normalized_words.append(word.encode('utf-8'))
-    return normalized_words
-
-# -- Outdated ---
-
-def split_flags(cmd, word):
-
-    if cmd == "tar" and all(c in "xtcrujzvf" for c in word):
-        for c in word:
-            yield "-" + c
-    elif cmd != "find":
-        if word.startswith("--"):
-
-            for flag in word.split('='):
-                yield flag
-
-        elif word.startswith("-"):
-
-            # turn -n1 into -n, 1
-            match = COMBINED_FLAG_AND_ARG.match(word)
-            if match:
-                yield match.group(1)
-                yield match.group(2)
-
-            # turns -xyz into -x, -y, -z
-            else:
-                for flag in word[1:]:
-                    yield "-" + flag
-        else:
-            yield word
-    else:
-        yield word
-
-
-def test_split_flags(cmd, flag, expected):
-    print(" --> testing flag splitting on {} {}... ".format(cmd, flag), end="")
-    result = list(split_flags(cmd, flag))
-    if result == expected:
-        print("ok {}".format(result))
-    else:
-        print("got {}".format(result))
-        assert result == expected # False
-
-def parse(code):
-    """
-    Yields all simple bash commands found in the code, e.g. ["ls", "-l"]
-    """
-    try:
-        parts = parse(code)
-    except Exception as e:
-        if DEBUG: print("warning: failed to parse {} ({}: {})".format(repr(code), type(e), e), file=sys.stderr)
-        return
-
-    if DEBUG: print("parsed: {}".format(repr(code)), file=sys.stderr)
-
-    for ast in parts:
-
-        class Visitor(ast.nodevisitor):
-            def __init__(self):
-                self.simplecmds = []
-            def visitcommand(self, cmd, parts):
-                if parts[0].kind == "word":
-                    cmd = parts[0].word
-                    args = []
-                    for part in parts[1:]:
-                        if part.kind == "word":
-                            if cmd in ["xargs", "time", "nohup", "nice"] and \
-                               part.word in ["find", "mv", "cp", "tar", "rm"]:
-                                self.simplecmds.append([cmd] + args)
-                                args = []
-                                cmd = part.word
-                            else:
-                                args += list(split_flags(cmd, part.word))
-                    self.simplecmds.append([cmd] + args)
-
-        v = Visitor()
-        v.visit(ast)
-        for cmd in v.simplecmds:
-            yield cmd
-
-
-if __name__ == "__main__":
-    # This lets you call `python bash.py "cmd"` to test the parser
-    DEBUG = True
-    test_split_flags("ls", "-lsh", ["-l", "-s", "-h"])
-    test_split_flags("tar", "--exclude=something", ["--exclude", "something"])
-    test_split_flags("tar", "czvf", ["-c", "-z", "-v", "-f"])
-    test_split_flags("find", "-iname", ["-iname"])
-    test_split_flags("xargs", "-n1", ["-n", "1"])
-    test_split_flags("nice", "-n20", ["-n", "20"])
-    for cmd in parse(" ".join(sys.argv[1:])):
-        print(cmd)
