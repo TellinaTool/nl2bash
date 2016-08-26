0c0be40b8dc424f618adaf242ae5f82407e54f80
Victoria Lin <xilin@cs.washington.edu>
2016-08-26 11:49:42 -0700

    change evaluation methdology to proposed

--- eval/ast_based.py
+++ eval/ast_based.py
@@ -4,11 +4,11 @@ sys.path.append("../bashlex")
 import normalizer
 
 def template_match(ast1, ast2):
-    temp1 = normalizer.to_template(ast1)
-    temp2 = normalizer.to_template(ast2)
+    temp1 = normalizer.to_template(ast1, loose_constraints=False)
+    temp2 = normalizer.to_template(ast2, loose_constraints=True)
     return temp1 == temp2
 
 def string_match(ast1, ast2):
-    str1 = normalizer.to_template(ast1, arc_type_only=False)
-    str2 = normalizer.to_template(ast2, arc_type_only=False)
+    str1 = normalizer.to_template(ast1, loose_constraints=False, arc_type_only=False)
+    str2 = normalizer.to_template(ast2, loose_constraints=True, arc_type_only=False)
     return str1 == str2
