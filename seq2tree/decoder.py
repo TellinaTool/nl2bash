62ef348a135e5631153aee72f883a944f3948152
Victoria Lin <xilin@cs.washington.edu>
2016-09-04 18:00:19 -0700

    merge

--- seq2tree/decoder.py
+++ seq2tree/decoder.py
@@ -358,6 +358,8 @@ class BasicTreeDecoder(Decoder):
         """
         :param batch_states: list of list of state tensors
         """
+        print(self.input.get_shape())
+        print(batch_states[0].get_shape())
         self.input = tf.concat(1, [self.input, batch_states[0]])
         self.back_pointers = tf.concat(1, [self.back_pointers, tf.expand_dims(batch_states[1], 1)])
         batch_states = tf.concat(1, batch_states[2:])
