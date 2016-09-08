ae4f6ec69289f82695076ae2a5bc9f62d8fa313a
Victoria Lin <xilin@cs.washington.edu>
2016-09-07 20:55:55 -0700

    merge

--- seq2tree/hyperparam_range.py
+++ seq2tree/hyperparam_range.py
@@ -3,5 +3,6 @@
 hyperparam_range = {
     "input_keep_prob": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
     "output_keep_prob": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
+    "num_layers": [2, 4, 8],
     "num_samples": [1024, 512]
 }
