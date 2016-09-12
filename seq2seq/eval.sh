9b072de0227b8f5c55e6d0d22f6578a66fdafaa9
Victoria Lin <xilin@cs.washington.edu>
2016-09-12 15:35:51 -0700

    remove shell scripts from repo

--- seq2seq/eval.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ARGS=${@:1}
-
-python translate.py --data_dir ../data/reader/seq2tree.by.command/ --train_dir ../model/seq2seq/lstm-attention-4/ --eval --nl_vocab_size 1500 --cm_vocab_size 1500 "${ARGS}"
