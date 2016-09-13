3019a77731283feea34401b35da88b182f4c0a8b
Victoria Lin <xilin@cs.washington.edu>
2016-09-12 22:21:11 -0700

    m

--- seq2tree/eval_tools.py
+++ seq2tree/eval_tools.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
@@ -328,10 +328,11 @@ def interactive_decode(sess, model, nl_vocab, rev_cm_vocab, FLAGS):
 
 
 def visualize_attn_masks(M, source, target, rev_nl_vocab, rev_cm_vocab, output_path):
-    fig = matplotlib.figure(figsize=[36,24])
-    fig.imshow(M)
+    fig = plt.imshow(M)
+
     nl = [rev_nl_vocab[x] for x in source]
-    cm = [rev_cm_vocab[x] for x in target]
-    fig.set_xticklabels(nl, rotation='vertical')
-    fig.set_yticklabels(cm, rotation='horizontal')
-    matplotlib.pylot.savefig(output_path)
+    rev_cm = [rev_cm_vocab[x] for x in reversed(target)]
+    fig.xticks(xrange(len(nl)), nl, rotation='vertical')
+    fig.yticks(xrange(len(rev_cm)), rev_cm, rotation='horizontal')
+
+    plt.savefig(output_path)
