d8cd867dfaf2b957ccda4717fce27f1861683fe3
Victoria Lin <xilin@cs.washington.edu>
2016-09-14 22:43:18 -0700

    mf

--- seq2tree/hyperparam_range.py
+++ seq2tree/hyperparam_range.py
@@ -2,7 +2,7 @@
 
 hyperparam_range = {
     "decoder_input_keep": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
-    "decoder_output_keep_": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
+    "decoder_output_keep": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
     "num_layers": [2, 4, 8],
     "num_samples": [1024, 512]
 }
