b38063ade21d03b70039a3a849dc1e1bf5e8898d
Victoria Lin <xilin@cs.washington.edu>
2016-09-14 22:39:15 -0700

    grid search for drop out rate

--- seq2tree/hyperparam_range.py
+++ seq2tree/hyperparam_range.py
@@ -1,8 +1,8 @@
 """Range of hyperparameters for grid search."""
 
 hyperparam_range = {
-    "input_keep_prob": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
-    "output_keep_prob": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
+    "decoder_input_keep": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
+    "decoder_output_keep_": [1.0, 0.9, 0.8, 0.7, 0.6, 0.5],
     "num_layers": [2, 4, 8],
     "num_samples": [1024, 512]
 }
