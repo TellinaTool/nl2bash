d0d506f9f3dc99a060b36121dc2fc3c8cd12eb25
Victoria Lin <xilin@cs.washington.edu>
2016-09-25 02:08:02 -0700

    pack knn model into a class

--- baselines/knn/knn.py
+++ baselines/knn/knn.py
@@ -1,171 +1,93 @@
-import os
 import heapq
 import math
-import sys
-
-data_dir = "."
 
 default_encoding_val = 999999999
 
-# utility function to load vector list from file
-def load_file_to_vec_list(path):
-  f = open(path, 'r')
-  lst = []
-  for line in f:
-    vec = line.split()
-    lst.append(map(lambda x : int(x), vec))
-  return lst
+class KNNModel(object):
+    def __init__(self):
+        self.idf = {}
+        self.term_occur_count = {}
 
-def load_dictionary(path, reverse = False):
-  f = open(path, 'r')
-  dictionary = {}
-  cnt = 0
-  for line in f:
-    if not reverse:
-      dictionary[cnt] = line.strip()
-    else:
-      dictionary[line.strip()] = cnt
-    cnt += 1
-  return dictionary
+    def train(self, train_set):
+        self.data_size = len(train_set)
 
-# the file containing traning nl vectors and cmd vectors
-cmd_vec_list = load_file_to_vec_list(os.path.join(data_dir, "train.ids1500.cm"))
-nl_vec_list = load_file_to_vec_list(os.path.join(data_dir, "train.ids1500.nl"))
+        # the file containing training nl vectors and cmd vectors
+        cmd_vec_list = [cmd_vec for _, _, _, cmd_vec in train_set]
+        nl_vec_list = [nl_vec for _, _, nl_vec, _ in train_set]
 
+        nl_vec_to_cmd_vec = {}
+        for i in range(self.data_size):
+            nl_vec_to_cmd_vec[str(nl_vec_list[i])] = cmd_vec_list[i]
 
-nl_vec_to_cmd_vec = {}
+        # calculate term weight based on tf-idf
+        for vec in nl_vec_list:
+            for k in set(vec):
+                if k in self.term_occur_count:
+                    self.term_occur_count[k] += 1
+                else:
+                    self.term_occur_count[k] = 1
 
-for i in range(len(cmd_vec_list)):
-  nl_vec_to_cmd_vec[str(nl_vec_list[i])] = cmd_vec_list[i]
+        self.nl_vec_list = nl_vec_list
+        self.nl_vec_to_cmd_vec = nl_vec_to_cmd_vec
 
+    def test(self, test_vec, k):
+        nl, score = self.find_k_nearest_neighbor(test_vec, self.nl_vec_list, k)[0]
+        cmd = self.nl_vec_to_cmd_vec[str(nl)]
+        return nl, cmd, score
 
-# dictionary to decode vectors to raw cmd or nl string
-cmd_dictionary = load_dictionary(os.path.join(data_dir, "vocab1500.cm"))
-nl_dictionary = load_dictionary(os.path.join(data_dir, "vocab1500.nl"))
+    # given a token, return idf
+    def get_idf(self, k):
+        count = 0
+        if k in self.term_occur_count:
+            count = self.term_occur_count[k]
+        return math.log(float(self.data_size) / (1 + count))
 
-# dictionary to encode nl/cmd string to bitvector
-cmd_reverse_dictionary = load_dictionary(os.path.join(data_dir, "vocab1500.cm"), reverse=True)
-nl_reverse_dictionary = load_dictionary(os.path.join(data_dir, "vocab1500.nl"), reverse=True)
+    def get_tf(self, term, term_vec):
+        r = 0
+        for k in term_vec:
+            if k == term:
+                r += 1
+        return r
 
-# calculate term weight based on tf-idf
+    # compute the distance between two natural language vectors
+    def compute_nl_vec_distance(self, nlvec1, nlvec2):
+        overlapped_tok = list(set(nlvec1) & set(nlvec2))
 
-idf = {}
-term_occur_count = {}
+        nlvec1_weighted_len = 0.
+        for k in set(nlvec1):
+            v = self.get_tf(k, nlvec1) * self.get_idf(k)
+            nlvec1_weighted_len += v * v
+        nlvec1_weighted_len = math.sqrt(nlvec1_weighted_len)
 
-for vec in nl_vec_list:
-  for k in set(vec):
-    if k in term_occur_count:
-      term_occur_count[k] += 1
-    else:
-      term_occur_count[k] = 1
+        nlvec2_weighted_len = 0.
+        for k in set(nlvec2):
+            v = self.get_tf(k, nlvec2) * self.get_idf(k)
+            nlvec2_weighted_len += v * v
+        nlvec2_weighted_len = math.sqrt(nlvec2_weighted_len)
 
-# given a token, return idf
-def get_idf(k):
-  count = 0
-  if k in term_occur_count:
-    count = term_occur_count[k]
-  return math.log(float(len(nl_vec_list)) / (1 + count) )
+        s = 0
 
-def get_tf(term, term_vec):
-  r = 0
-  for k in term_vec:
-    if k == term:
-      r = r + 1
-  return r
+        for k in overlapped_tok:
+            v1 = self.get_tf(k, nlvec1) * self.get_idf(k)
+            v2 = self.get_tf(k, nlvec2) * self.get_idf(k)
+            s += v1 * v2
 
-token_weight_func = {}
+        return s / (nlvec2_weighted_len * nlvec1_weighted_len)
+
+    # sort and find top k most rated bitvectors
+    def find_k_nearest_neighbor(self, vec, vec_list, k):
+        scored_vec_list = map(lambda v: (v, self.compute_nl_vec_distance(vec, v)),
+                              vec_list)
+        return heapq.nlargest(k, scored_vec_list, key=lambda x: x[1])
+
 
 # take a vector and a dictionary to decode a vector to a string
 def decode_vec_to_str(vec, dictionary):
-  return reduce(lambda x, y: x + " " + y, map(lambda x : dictionary[x], vec), "")
+    return reduce(lambda x, y: x + " " + y, map(lambda x: dictionary[x], vec), "")
+
 
 # encode a string to a vector based on the dictionary
 def encode_str_to_vec(s, dictionary):
-  return filter(lambda x :  x != default_encoding_val, 
-                  map(lambda x : dictionary[x] if (x in dictionary) else default_encoding_val, 
-                        s.split()))
-
-# sort and find top k most rated bitvectors
-def find_k_nearest_neighbor(vec, vec_list, k):
-  scored_vec_list = map(lambda v : (v, compute_nl_vec_distance(vec, v)), vec_list)
-  return heapq.nlargest(k, scored_vec_list, key=lambda x:x[1])
-
-# compute the distance between two natural language vectors
-def compute_nl_vec_distance(nlvec1, nlvec2):
-  overlapped_tok = list(set(nlvec1) & set(nlvec2))
-
-  nlvec1_weighted_len = 0.
-  for k in set(nlvec1):
-    v = get_tf(k, nlvec1) * get_idf(k)
-    nlvec1_weighted_len += v * v
-  nlvec1_weighted_len = math.sqrt(nlvec1_weighted_len)
-
-  nlvec2_weighted_len = 0.
-  for k in set(nlvec2):
-    v = get_tf(k, nlvec2) * get_idf(k)
-    nlvec2_weighted_len += v * v
-  nlvec2_weighted_len = math.sqrt(nlvec2_weighted_len)
-
-  s = 0
-
-  for k in overlapped_tok:
-    v1 = get_tf(k, nlvec1) * get_idf(k)
-    v2 = get_tf(k, nlvec2) * get_idf(k)
-    s += v1 * v2
-
-  return s / (nlvec2_weighted_len * nlvec1_weighted_len)
-
-
-  #return float(len(overlapped_tok)) / float(((len(nlvec1) + len(nlvec2)) / 2))
-
-def main():
-  #test_vec = [26, 12, 10, 11, 15, 17, 28, 171, 18, 339]
-
-  #print "[command] ", decode_vec_to_str(test_vec, nl_dictionary)
-
-  # find k nearest neighbor
-  #knn = find_k_nearest_neighbor(test_vec, nl_vec_list, 1)
-
-  #for p in knn:
-  #  print "[nn vec] ", p
-    # print the decoding result of these filters
-  #  print "[nearest neighbor] ", decode_vec_to_str(p[0], nl_dictionary)
-
-  sys.stdout = open('result.txt', 'w')
-
-  # the file containing traning nl vectors and cmd vectors
-  test_cmd_vec_list = load_file_to_vec_list(os.path.join(data_dir, "dev.ids1500.cm"))
-  test_nl_vec_list = load_file_to_vec_list(os.path.join(data_dir, "dev.ids1500.nl"))
-
-
-  for i in range(len(test_nl_vec_list)):
-    test_vec = test_nl_vec_list[i]
-    cmd_vec = test_cmd_vec_list[i]
-
-    nl = find_k_nearest_neighbor(test_vec, nl_vec_list, 1)[0][0]
-    cmd = nl_vec_to_cmd_vec[str(nl)]
-
-    print "[text-case ", i, "] ========================================================="
-    print "  [original-pair]"
-    print "     ", decode_vec_to_str(test_vec, nl_dictionary)
-    print "     ", decode_vec_to_str(cmd_vec, cmd_dictionary)
-    print "  [new-pair]"
-    print "     ", decode_vec_to_str(nl, nl_dictionary)
-    print "     ", decode_vec_to_str(cmd, cmd_dictionary)
-    print decode_vec_to_str(cmd, cmd_dictionary)
-
-
-if __name__ == '__main__':
-  main()
-
-
-
-
-
-
-
-
-
-
-
-
+    return filter(lambda x: x != default_encoding_val,
+                  map(lambda x: dictionary[x] if (x in dictionary) else default_encoding_val,
+                      s.split()))
