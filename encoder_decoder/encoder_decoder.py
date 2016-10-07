ba07bfee8432fe429df1e176048e42113cfe61ab
Victoria Lin <xilin@cs.washington.edu>
2016-10-07 12:23:59 -0700

    use hidden features for attention

--- encoder_decoder/encoder_decoder.py
+++ encoder_decoder/encoder_decoder.py
@@ -290,8 +290,6 @@ class EncoderDecoderModel(graph_utils.NNModel):
             batch_attn_masks.append(batch_attn_mask)
             if self.use_copy:
                 raise NotImplementedError
-        print(encoder_size)
-        print(batch_attn_masks)
 
         # Batch decoder inputs are re-indexed decoder_inputs.
         for length_idx in xrange(decoder_size):
