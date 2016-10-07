9e90489fdd37d2186ccb9571b1a546193cec9656
Victoria Lin <xilin@cs.washington.edu>
2016-10-07 11:42:42 -0700

    add pad symbol mask to attention

--- encoder_decoder/encoder_decoder.py
+++ encoder_decoder/encoder_decoder.py
@@ -60,12 +60,15 @@ class EncoderDecoderModel(graph_utils.NNModel):
     def define_graph(self, forward_only):
         # Feeds for inputs.
         self.encoder_inputs = []  # encoder inputs.
+        self.encoder_attn_masks = []      # mask out PAD symbols in the encoder
         self.decoder_inputs = []  # decoder inputs (always start with "root").
         self.target_weights = []  # weights at each position of the target sequence.
 
         for i in xrange(self.max_source_length):
             self.encoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
-                                                      name="encoder{0}".format(i)))
+                                       name="encoder{0}".format(i)))
+            self.encoder_attn_masks.append(tf.placeholder(tf.int32, shape=[None],
+                                           name="attn_mask{0}".format(i)))
         for i in xrange(self.max_target_length + 1):
             self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
                                                       name="decoder{0}".format(i)))
@@ -109,7 +112,8 @@ class EncoderDecoderModel(graph_utils.NNModel):
                 bucket_output_symbols, bucket_output_logits, bucket_losses, \
                 attn_mask = \
                     self.encode_decode(
-                        self.encoder_inputs[:bucket[0]], self.source_embeddings(),
+                        self.encoder_inputs[:bucket[0]], self.encoder_attn_masks[:bucket[0]],
+                        self.source_embeddings(),
                         self.decoder_inputs[:bucket[1]], self.target_embeddings(),
                         forward_only=forward_only
                     )
@@ -166,8 +170,8 @@ class EncoderDecoderModel(graph_utils.NNModel):
         self.decoder = None
 
 
-    def encode_decode(self, encoder_inputs, source_embeddings, decoder_inputs,
-                      target_embeddings, forward_only):
+    def encode_decode(self, encoder_inputs, encoder_attn_masks, source_embeddings,
+                      decoder_inputs, target_embeddings, forward_only):
         encoder_outputs, encoder_state = self.encoder.define_graph(encoder_inputs,
                                                                source_embeddings)
 
@@ -184,7 +188,7 @@ class EncoderDecoderModel(graph_utils.NNModel):
             output_symbols, output_logits, outputs, state, attn_mask = \
                 self.decoder.define_graph(
                     encoder_state, decoder_inputs, target_embeddings,
-                    attention_states, num_heads=1,
+                    attention_states, encoder_attn_masks, num_heads=1,
                     feed_previous=forward_only)
         else:
             output_symbols, output_logits, outputs, state = \
@@ -268,6 +272,7 @@ class EncoderDecoderModel(graph_utils.NNModel):
 
         # create batch-major vectors
         batch_encoder_inputs = []
+        batch_attn_masks = []
         batch_decoder_inputs = []
         batch_weights = []
 
@@ -276,6 +281,13 @@ class EncoderDecoderModel(graph_utils.NNModel):
             batch_encoder_inputs.append(
                 np.array([padded_encoder_inputs[batch_idx][length_idx]
                           for batch_idx in xrange(batch_size)], dtype=np.int32))
+
+            batch_attn_mask = np.ones(batch_size, dtype=np.float32)
+            for batch_idx in xrange(batch_size):
+                source = padded_decoder_inputs[batch_idx][length_idx]
+                if source == data_utils.PAD_ID:
+                    batch_attn_mask[batch_idx] = 0.0
+            batch_attn_masks.append(batch_attn_mask)
             if self.use_copy:
                 raise NotImplementedError
 
@@ -301,10 +313,11 @@ class EncoderDecoderModel(graph_utils.NNModel):
         if self.use_copy:
             raise NotImplementedError
         else:
-            return batch_encoder_inputs, batch_decoder_inputs, batch_weights
+            return batch_encoder_inputs, batch_attn_masks,\
+                   batch_decoder_inputs, batch_weights
 
 
-    def get_batch(self, data, bucket_id):
+    def get_batch(self, data, bucket_id, copy_data=None):
         """Get a random batch of data from the specified bucket, prepare for step.
 
         To feed data in step(..) it must be a list of batch-major vectors, while
@@ -330,38 +343,13 @@ class EncoderDecoderModel(graph_utils.NNModel):
 
             # Encoder inputs are padded and then reversed.
             encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
-            # encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
             encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
 
             decoder_pad_size = decoder_size - len(decoder_input)
             decoder_inputs.append(decoder_input + [data_utils.PAD_ID] * decoder_pad_size)
 
-        # Now we create batch-major vectors from the data selected above.
-        batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
-
-        # Batch encoder inputs are just re-indexed encoder_inputs.
-        for length_idx in xrange(encoder_size):
-            batch_encoder_inputs.append(
-                np.array([encoder_inputs[batch_idx][length_idx]
-                          for batch_idx in xrange(self.batch_size)], dtype=np.int32))
-
-        # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
-        for length_idx in xrange(decoder_size):
-            batch_decoder_inputs.append(
-                np.array([decoder_inputs[batch_idx][length_idx]
-                          for batch_idx in xrange(self.batch_size)], dtype=np.int32))
-
-            # Create target_weights to be 0 for targets that are padding.
-            batch_weight = np.ones(self.batch_size, dtype=np.float32)
-            for batch_idx in xrange(self.batch_size):
-                # We set weight to 0 if the corresponding target is a PAD symbol.
-                # The corresponding target is decoder_input shifted by 1 forward.
-                if length_idx < decoder_size - 1:
-                    target = decoder_inputs[batch_idx][length_idx + 1]
-                if length_idx == decoder_size - 1 or target == data_utils.PAD_ID:
-                    batch_weight[batch_idx] = 0.0
-            batch_weights.append(batch_weight)
-        return batch_encoder_inputs, batch_decoder_inputs, batch_weights
+        return self.format_example(encoder_inputs, decoder_inputs, copy_data=None,
+                                   bucket_id=bucket_id)
 
 
     def step(self, session, formatted_example, bucket_id=-1, forward_only=False):
@@ -376,10 +364,10 @@ class EncoderDecoderModel(graph_utils.NNModel):
         """
         # Unwarp data tensors
         if self.use_copy:
-            encoder_inputs, decoder_inputs, target_weights, \
+            encoder_inputs, decoder_inputs, attn_masks, target_weights, \
             original_encoder_inputs, original_decoder_inputs, copy_masks = formatted_example
         else:
-            encoder_inputs, decoder_inputs, target_weights = formatted_example
+            encoder_inputs, decoder_inputs, attn_masks, target_weights = formatted_example
 
         # Check if the sizes match.
         if bucket_id == -1:
@@ -402,6 +390,7 @@ class EncoderDecoderModel(graph_utils.NNModel):
         input_feed = {}
         for l in xrange(encoder_size):
             input_feed[self.encoder_inputs[l].name] = encoder_inputs[l]
+            input_feed[self.encoder_attn_masks[l].name] = attn_masks[l]
         for l in xrange(decoder_size):
             input_feed[self.decoder_inputs[l].name] = decoder_inputs[l]
             input_feed[self.target_weights[l].name] = target_weights[l]
