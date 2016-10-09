ee6a527dd766d85809bcde5019e25450cde823bb
Victoria Lin <xilin@cs.washington.edu>
2016-10-09 16:53:00 -0700

    merge

--- eval/eval_tools.py
+++ eval/eval_tools.py
@@ -34,7 +34,7 @@ def eval_set(model, dataset, rev_nl_vocab, FLAGS, verbose=True):
     total_top10_dist = 0.0
     num_eval = 0
 
-    cmd_parser = data_utils.bash_parser if FLAGS else data_utils.parse_brackets
+    cmd_parser = data_tools.bash_parser if FLAGS else data_utils.parse_brackets
 
     use_bucket = False if model == "knn" else True
     grouped_dataset = data_utils.group_data_by_nl(dataset, use_bucket=use_bucket)
