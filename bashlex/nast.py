64c3407e6c25e0ffab12bc24b2caee3c2fbea026
Victoria Lin <xilin@cs.washington.edu>
2016-10-14 17:42:42 -0700

    mf

--- bashlex/nast.py
+++ bashlex/nast.py
@@ -228,7 +228,7 @@ class HeadCommandNode(Node):
             for child in node.children:
                 if child.is_argument():
                     if repl_str in child.value:
-                        child.value.replace(repl_str, norm)
+                        child.value = child.value.replace(repl_str, norm)
                         if child.value == norm:
                             child.arg_type = "ReservedWord"
                 else:
