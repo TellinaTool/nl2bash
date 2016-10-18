70152871431b0bbf738f7dc55f2a10c7040ce88a
Victoria Lin <xilin@cs.washington.edu>
2016-10-18 00:55:03 -0700

    mf

--- encoder_decoder/seq2seq/rnn_decoder.py
+++ encoder_decoder/seq2seq/rnn_decoder.py
@@ -65,6 +65,7 @@ class RNNDecoder(decoder.Decoder):
                 decoder_cell = beam_decoder.wrap_cell(decoder_cell, self.output_projection)
             elif self.decoding_algorithm == "greedy":
                 if self.use_attention:
+                    encoder_attn_masks = tf.concat(1, encoder_attn_masks)
                     decoder_cell = decoder.AttentionCellWrapper(decoder_cell,
                                                             attention_states,
                                                             encoder_attn_masks,
