1574fb3535026f0d3e3361189a3e64ad47d9c5c7
Victoria Lin <xilin@cs.washington.edu>
2016-10-17 19:23:14 -0700

    mf

--- encoder_decoder/seq2seq/rnn_decoder.py
+++ encoder_decoder/seq2seq/rnn_decoder.py
@@ -136,7 +136,7 @@ class RNNDecoder(decoder.Decoder):
             attn_masks = tf.reshape(attn_masks, [self.batch_size,
                                                  self.beam_size,
                                                  len(decoder_inputs),
-                                                 attention_states.get_shape()[1]])
+                                                 attention_states.get_shape()[1].value])
             if self.use_attention:
                 return top_k_outputs, top_k_logits, outputs, state, attn_masks
             else:
