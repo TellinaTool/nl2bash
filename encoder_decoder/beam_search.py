fix bug in beam search attention mask

encoder_decoder/beam_search.py
@@ -184,7 +184,7 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
         self._done_mask = tf.tile(self._done_mask, [full_size, 1])
 
 
-    def __call__(self, inputs, state, attns=None, scope=None, alpha=1.05):
+    def __call__(self, inputs, state, attn_masks=None, scope=None, alpha=1.05):
         (
             past_cand_symbols,  # [batch_size, max_len]
             past_cand_logprobs, # [batch_size]
@@ -203,8 +203,8 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
 
         cell_inputs = inputs
         if self.use_attention:
-            cell_outputs, raw_cell_state, attns, attn_mask = \
-                self.cell(cell_inputs, past_cell_state, attns, scope)
+            cell_outputs, raw_cell_state, attn_masks = \
+                self.cell(cell_inputs, past_cell_state, attn_masks, scope)
         else:
             cell_outputs, raw_cell_state = \
                 self.cell(cell_inputs, past_cell_state, scope)
@@ -247,7 +247,7 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
         parent_refs = parent_refs + self.parent_refs_offsets
 
         symbols_history = tf.gather(past_beam_symbols, parent_refs)
-        attn_mask = tf.gather(attn_mask, parent_refs)
+        attn_masks = tf.gather(attn_masks, parent_refs)
         beam_symbols = tf.concat(1, [symbols_history[:,1:], tf.reshape(symbols, [-1, 1])])
         self.seq_len = tf.gather(self.seq_len, parent_refs) + \
                        tf.cast(tf.not_equal(tf.reshape(symbols, [-1]), 
@@ -277,7 +277,7 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
                 beam_symbols,
                 beam_logprobs,
                 cell_state,
-            ), attns, attn_mask
+            ), attn_masks
         else:
             return cell_outputs, (
                 cand_symbols,

encoder_decoder/decoder.py
@@ -80,17 +80,12 @@ class AttentionCellWrapper(tf.nn.rnn_cell.RNNCell):
         self.attention_vars = True
         return attns, attn_mask
 
-    def __call__(self, input_embedding, state, attns, scope=None):
+    def __call__(self, input_embedding, state, attn_masks, scope=None):
         dim = state.get_shape()[1].value
         with tf.variable_scope("AttnInputProjection"):
             if self.attention_cell_vars:
                 tf.get_variable_scope().reuse_variables()
-            # attention mechanism on cell and hidden states
-            # attn_vec_dim = self.v[0].get_shape()[0].value
-            # attns.set_shape([None, self.num_heads * attn_vec_dim])
-            # x = tf.nn.rnn_cell._linear([input_embedding] + [attns], self.dim, True)
-            x = input_embedding
-            _, state = self.cell(x, state, scope)
+            _, state = self.cell(input_embedding, state, scope)
             attns, attn_mask = self.attention(state)
 
         with tf.variable_scope("AttnStateProjection"):
@@ -106,4 +101,6 @@ class AttentionCellWrapper(tf.nn.rnn_cell.RNNCell):
             output = tf.nn.rnn_cell._linear(attn_state, dim, True)
 
         self.attention_cell_vars = True
-        return output, state, attns, attn_mask
+
+        tf.concat(1, [attn_masks, tf.expand_dims(attn_mask, 1)])
+        return output, state, attn_masks

encoder_decoder/seq2seq/rnn_decoder.py
@@ -28,7 +28,6 @@ class RNNDecoder(decoder.Decoder):
             decoder_cell, decoder_scope = self.decoder_cell()
             state = encoder_state
             outputs = []
-            attn_masks = []
 
             # applying cell wrappers: ["attention", "beam"]
             if self.decoding_algorithm == "beam_search":
@@ -73,18 +72,12 @@ class RNNDecoder(decoder.Decoder):
             # initial attention state
             if self.use_attention:
                 batch_size = tf.shape(attention_states)[0]
-                attn_dim = tf.shape(attention_states)[2]
-                batch_attn_size = tf.pack([batch_size, attn_dim])
-                attns = tf.concat(1, [tf.zeros(batch_attn_size, dtype=tf.float32)
-                                      for _ in xrange(num_heads)])
-                if initial_state_attention:
-                    attns, _ = decoder_cell.attention(encoder_state)
+                attn_length = tf.shape(attention_states)[1]
+                attn_masks = tf.zeros([batch_size, 0, attn_length])
 
             for i, input in enumerate(decoder_inputs):
                 if self.decoding_algorithm == "beam_search":
                     input = beam_decoder.wrap_input(input)
-                    if self.use_attention:
-                        attns = beam_decoder.wrap_input(attns)
                
                 if i > 0:
                     scope.reuse_variables()
@@ -112,19 +105,12 @@ class RNNDecoder(decoder.Decoder):
                 input_embedding = tf.nn.embedding_lookup(embeddings, input)
 
                 if self.use_attention:
-                    output, state, attns, attn_mask = \
-                        decoder_cell(input_embedding, state, attns, scope=decoder_scope)
-                    attn_length = tf.shape(attention_states)[1]
-                    if self.decoding_algorithm == "beam_search":
-                        attn_mask = tf.reshape(attn_mask, [-1, self.beam_size, attn_length])
-                    attn_masks.append(tf.expand_dims(attn_mask, 1))
+                    output, state, attn_masks = \
+                        decoder_cell(input_embedding, state, attn_masks, scope=decoder_scope)
                 else:
                     output, state = decoder_cell(input_embedding, state, scope=decoder_scope)
                 # record output state to compute the loss.
                 outputs.append(output)
-
-        if self.use_attention:
-             attn_masks = tf.concat(1, attn_masks)
 
         # Beam-search output
         if self.decoding_algorithm == "beam_search":