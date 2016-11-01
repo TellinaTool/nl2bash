ef9d78734783e17e6d578b8eb64e31436728047b
Victoria Lin <xilin@cs.washington.edu>
2016-11-01 15:19:45 -0700

    mf

--- encoder_decoder/graph_utils.py
+++ encoder_decoder/graph_utils.py
@@ -56,7 +56,8 @@ def create_model(session, FLAGS, model_constructor, buckets, forward_only,
     print("model_dir={}".format(FLAGS.model_dir))
 
     if forward_only:
-        FLAGS.batch_size = 100
+        if not FLAGS.demo:
+            FLAGS.batch_size = 100
         params["batch_size"] = 100
         params["attention_input_keep"] = 1.0
         params["attention_output_keep"] = 1.0
