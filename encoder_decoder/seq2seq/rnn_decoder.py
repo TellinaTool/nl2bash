cb7866547284d8b9a94561cd8adcc595e5f168b0
Victoria Lin <xilin@cs.washington.edu>
2016-10-31 22:22:59 -0700

    mf

--- encoder_decoder/seq2seq/rnn_decoder.py
+++ encoder_decoder/seq2seq/rnn_decoder.py
@@ -64,8 +64,8 @@ class RNNDecoder(decoder.Decoder):
                                                             self.attention_output_keep,
                                                             num_heads,
                                                             reuse_variables)
-                past_output_symbols = decoder_inputs[0]
-                past_output_logits = decoder_inputs[0] * 0
+                past_output_symbols = tf.cast(decoder_inputs[0], tf.int64)
+                past_output_logits = tf.cast(decoder_inputs[0] * 0, tf.float32)
 
             for i, input in enumerate(decoder_inputs):
                 if self.decoding_algorithm == "beam_search":
