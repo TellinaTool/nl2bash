b441df27152897598285bc10e10f7f2d023768c1
Victoria Lin <xilin@cs.washington.edu>
2016-11-02 15:31:10 -0700

    mf

--- encoder_decoder/encoder_decoder.py
+++ encoder_decoder/encoder_decoder.py
@@ -222,8 +222,9 @@ class EncoderDecoderModel(graph_utils.NNModel):
         else:
             output_symbols, output_logits, outputs, state = \
                 self.decoder.define_graph(
-                encoder_state, decoder_inputs, target_embeddings,
-                feed_previous=forward_only, reuse_variables=reuse_variables)
+                    encoder_state, decoder_inputs, target_embeddings,
+                    beam_decoder=beam_decoder, feed_previous=forward_only, 
+                    reuse_variables=reuse_variables)
 
         # Losses.
         if self.use_attention:
