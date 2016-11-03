33637d82ca6dcf7a5ccc0787b9257567093c666b
Victoria Lin <xilin@cs.washington.edu>
2016-11-02 19:26:50 -0700

    switch to absolute import

--- encoder_decoder/seq2tree/seq2tree_model.py
+++ encoder_decoder/seq2tree/seq2tree_model.py
@@ -4,12 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from encoder_decoder import EncoderDecoderModel
-import encoder, tree_decoder
+from encoder_decoder.encoder_decoder import EncoderDecoderModel
+from encoder_decoder import encoder
+from seq2tree import tree_decoder
 
 
 class Seq2TreeModel(EncoderDecoderModel):
