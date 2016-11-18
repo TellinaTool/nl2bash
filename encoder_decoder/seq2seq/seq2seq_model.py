fd442f6c200248ac5c680ac1a74275b564edbac1
Victoria Lin <xilin@cs.washington.edu>
2016-11-17 17:52:03 -0800

    mf

--- encoder_decoder/seq2seq/seq2seq_model.py
+++ encoder_decoder/seq2seq/seq2seq_model.py
@@ -5,15 +5,10 @@ from __future__ import division
 from __future__ import print_function
 
 import os, sys
-if os.path.realpath(__file__).startswith(os.getcwd()):
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    from encoder_decoder import EncoderDecoderModel
-    import encoder
-    import rnn_decoder    
-else:
-    from encoder_decoder.encoder_decoder import EncoderDecoderModel
-    from encoder_decoder import encoder
-    from seq2seq import rnn_decoder
+
+from encoder_decoder.encoder_decoder import EncoderDecoderModel
+from encoder_decoder import encoder
+from seq2seq import rnn_decoder
 
 
 class Seq2SeqModel(EncoderDecoderModel):
