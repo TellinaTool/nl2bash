fix bug with gt_logprobs computation in bso

encoder_decoder/seq2seq/rnn_decoder.py
@@ -37,6 +37,7 @@ class RNNDecoder(decoder.Decoder):
             beam_attn_masks = []
             outputs = []
             bso_losses = []
+            gt_logprobs = []
 
             full_size = self.batch_size * self.beam_size
 
@@ -123,7 +124,7 @@ class RNNDecoder(decoder.Decoder):
                 # compute search-based training loss
                 if not forward_only:
                     W, b = self.output_projection
-                    projected_output = tf.nn.softmax(tf.matmul(output, W) + b)
+                    projected_output = tf.nn.log_softmax(tf.matmul(output, W) + b)
 
                     (
                         past_cand_symbols,  # [batch_size, max_len]
@@ -138,13 +139,25 @@ class RNNDecoder(decoder.Decoder):
                     # compute the loss in current step 
                     # (notice that one batch has only one loss, hence the notion of "compressed")
                     search_complete = tf.equal(target_weights[i+1], 0)
-                    gt_probs = tf.nn.embedding_lookup(tf.reshape(projected_output, [-1]),
-                                   tf.range(self.batch_size) * self.target_vocab_size +
-                                   partial_target_symbols[:, -1]
-                               )
+                    gt_logprobs.append(tf.nn.embedding_lookup(tf.reshape(projected_output, [-1]),
+                                       tf.range(self.batch_size) * self.target_vocab_size +
+                                       partial_target_symbols[:, -1]
+                                   ))
+                    # length normalization
+                    ground_truth_logprobs = tf.div(tf.reduce_sum(
+                                                    tf.reshape(
+                                                        gt_logprobs,
+                                                        [self.batch_size, -1]
+                                                    ),
+                                                1),
+                                                tf.reduce_sum(
+                                                    partial_target_weights,
+                                                    1
+                                                )
+                                         )
                     beam_logprobs = tf.reshape(past_beam_logprobs, [-1, self.beam_size])
                     pred_logprobs = tf.select(search_complete, beam_logprobs[:, 0], beam_logprobs[:, -1])
-                    step_loss = tf.maximum(self.margin - (gt_probs - tf.exp(pred_logprobs)), 0)
+                    step_loss = tf.maximum(self.margin - (tf.exp(ground_truth_logprobs) - tf.exp(pred_logprobs)), 0)
                     bso_losses.append(step_loss)
 
                     # resume using reference search states if ground_truth fell off beam
@@ -174,7 +187,7 @@ class RNNDecoder(decoder.Decoder):
                     first_in_beam_mask = tf.equal(tf.range(full_size) % self.beam_size, 0)
                     ground_truth_logprobs = tf.select(
                         first_in_beam_mask,
-                        beam_decoder.wrap_input(tf.log(gt_probs)),
+                        beam_decoder.wrap_input(ground_truth_logprobs),
                         tf.fill([full_size], -1e18)
                     )
                     beam_logprobs = tf.select(in_beam, past_beam_logprobs, ground_truth_logprobs)