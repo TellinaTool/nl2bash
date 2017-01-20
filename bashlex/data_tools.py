87a43bcddb55159336123d478765c853433b5c63
Victoria Lin <xilin@cs.washington.edu>
2017-01-19 16:53:11 -0800

    Path fill bug fix

--- bashlex/data_tools.py
+++ bashlex/data_tools.py
@@ -280,9 +280,10 @@ def fill_arguments(node, arguments):
                 if arguments['File']:
                     fill_argument('File')
                     return
-                if arguments['Regex']:
+                """if arguments['Regex']:
                     fill_argument('Regex')
                     return
+                """
                 node.value = '.'
             elif node.arg_type == 'Directory':
                 if arguments['File']:
