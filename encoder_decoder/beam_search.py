840ea74a90aaaead351a34da6439b940862047d0
Victoria Lin <xilin@cs.washington.edu>
2017-02-01 09:58:36 -0800

    fix bug in getting beam decoder hidden states

--- encoder_decoder/beam_search.py
+++ encoder_decoder/beam_search.py
@@ -367,7 +367,7 @@ class BeamDecoderCellWrapper(tf.nn.rnn_cell.RNNCell):
             cand_logprobs,
             beam_symbols,
             beam_logprobs,
-            cell_state
+            tf.expand_dims(cell_state, 1)
         )
 
     def zero_state(self, batch_size_times_beam_size, dtype):
