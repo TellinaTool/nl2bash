ab1b36834188683c81213702922286eced73439e
Victoria Lin <xilin@cs.washington.edu>
2017-01-31 22:37:53 -0800

    mf

--- nlp_tools/slot_filling.py
+++ nlp_tools/slot_filling.py
@@ -8,7 +8,6 @@ import collections, copy, datetime, re
 import numpy as np
 
 from . import constants, tokenizer
-from .constants import strip
 from bashlex.data_tools import bash_tokenizer
 
 # --- Slot-filler Alignment --- #
@@ -519,3 +518,27 @@ def heuristic_slot_filling(node, entities):
             return False
 
     return True
+
+def strip(pattern):
+    # special_start_1c_re = re.compile(r'^[\"\'\*\\\/\.-]]')
+    # special_start_2c_re = re.compile(r'^\{\}')
+    # special_end_1c_re = re.compile(r'[\"\'\\\/\$\*\.-]$')
+    # special_end_2c_re = re.compile(r'(\\n|\{\})$')
+    while len(pattern) > 1 and \
+            pattern[0] in ['"', '\'', '*', '\\', '/', '.', '-', '{', '}']:
+        pattern = pattern[1:]
+    while len(pattern) > 1 and \
+            pattern[-1] in ['"', '\'', '\\', '/', '$', '*', '.', '-',
+                            '{', '}']:
+        pattern = pattern[:-1]
+    special_start_re = re.compile(r'^\{\}')
+    special_end_re = re.compile(r'(\\n|\{\})$')
+    while len(pattern) > 2 and re.search(special_end_re, pattern):
+        pattern = pattern[:-2]
+    while len(pattern) > 1 and \
+            pattern[0] in ['"', '\'', '*', '\\', '/', '.', '-']:
+        pattern = pattern[1:]
+    while len(pattern) > 1 and \
+            pattern[-1] in ['"', '\'', '\\', '/', '$', '*', '.', '-']:
+        pattern = pattern[:-1]
+    return pattern
