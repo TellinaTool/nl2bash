fix bug on beam decoder loss computation

encoder_decoder/framework.py
@@ -224,21 +224,24 @@ class EncoderDecoderModel(graph_utils.NNModel):
         if self.training_algorithm == "bso":
             output_symbols, output_logits, outputs, state, \
                 attn_mask, bso_losses = self.decoder.define_bso_graph(
-                encoder_state, decoder_inputs, target_weights, target_embeddings,
-                encoder_attn_masks, attention_states, num_heads=1,
-                beam_decoder=beam_decoder, forward_only=forward_only,
-                reuse_variables=reuse_variables
-            )
+                    encoder_state, decoder_inputs, target_weights, target_embeddings,
+                    encoder_attn_masks, attention_states, num_heads=1,
+                    beam_decoder=beam_decoder, forward_only=forward_only,
+                    reuse_variables=reuse_variables
+                )
         else:
             output_symbols, output_logits, outputs, state, \
                 attn_mask = self.decoder.define_graph(
-                encoder_state, decoder_inputs, target_embeddings,
-                encoder_attn_masks, attention_states, num_heads=1,
-                beam_decoder=beam_decoder, forward_only=forward_only,
-                reuse_variables=reuse_variables
-            )
+                    encoder_state, decoder_inputs, target_embeddings,
+                    encoder_attn_masks, attention_states, num_heads=1,
+                    beam_decoder=beam_decoder, forward_only=forward_only,
+                    reuse_variables=reuse_variables
+                )
 
         if forward_only:
+            if self.decoding_algorithm == 'beam_search':
+                outputs = tf.gather(outputs, tf.range(0,
+                    self.batch_size*self.beam_size, self.beam_size))
             encoder_decoder_loss = graph_utils.sequence_loss(
                                        outputs, targets, target_weights,
                                        graph_utils.softmax_loss(

encoder_decoder/seq2seq/rnn_decoder.py
@@ -154,7 +154,7 @@ class RNNDecoder(decoder.Decoder):
                                 for top_k_logit in top_k_logits]
                 if self.use_attention:
                     attn_masks = tf.reshape(attn_masks, [self.batch_size, self.beam_size,
-                                            len(decoder_inputs), attention_states.get_shape()[1].value])
+                            len(decoder_inputs), attention_states.get_shape()[1].value])
                 outputs = [tf.squeeze(s, squeeze_dims=[1]) for s in tf.split(
                     1, past_cell_states.get_shape()[1], past_cell_states)[1:]]
                 return top_k_outputs, top_k_logits, outputs, state, attn_masks