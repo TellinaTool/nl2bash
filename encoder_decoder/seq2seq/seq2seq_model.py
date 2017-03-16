c8d6bcbd641859b9652ff1c91dd9a72e2b8afa07
Victoria X. Lin <xilin@cs.washington.edu>
2017-03-16 09:37:53 -0700

    Update seq2seq_model.py

--- encoder_decoder/seq2seq/seq2seq_model.py
+++ encoder_decoder/seq2seq/seq2seq_model.py
@@ -4,16 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from encoder_decoder.framework import EncoderDecoderModel
-from encoder_decoder import encoder
-
-try:
-    from seq2seq import rnn_decoder
-except ImportError:
-    try:
-        from . import rnn_decoder
-    except ImportError:
-        import rnn_decoder
+from net import encoder
+from net.encoder_decoder import EncoderDecoderModel
+from . import rnn_decoder
 
 class Seq2SeqModel(EncoderDecoderModel):
     """Sequence-to-sequence model with attention and for multiple buckets.
