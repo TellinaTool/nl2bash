4a5d474f960be71c5134e5ce8819ed9531293e50
Victoria Lin <xilin@cs.washington.edu>
2017-03-16 11:49:23 -0700

    correct import errors

--- encoder_decoder/data_stats.py
+++ encoder_decoder/data_stats.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
 from encoder_decoder.data_utils import *
 
 def ratio(ll, x):
