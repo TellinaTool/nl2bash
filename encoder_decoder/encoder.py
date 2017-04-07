ab568bd8741a9ac0bebeaa41a39da0630d25879c
Victoria Lin <xilin@cs.washington.edu>
2017-04-07 10:39:41 -0700

    add a few more print statements

--- encoder_decoder/encoder.py
+++ encoder_decoder/encoder.py
@@ -151,6 +151,8 @@ class BiRNNEncoder(Encoder):
         self.fw_cell = self.forward_cell()
         self.bw_cell = self.backward_cell()
         self.output_dim = 2 * self.input_dim
+        print("Encoder input dimension = {}".format(self.input_dim))
+        print("Encoder output dimension = {}".format(self.output_dim))
 
     def define_graph(self, encoder_inputs):
         # Each rnn in the bi-directional encoder have dimension which is half
