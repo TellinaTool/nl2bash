d3e34c54f81b1438b8870a025baa1afc90935f12
Victoria Lin <xilin@cs.washington.edu>
2017-04-12 02:23:03 -0700

    mf

--- encoder_decoder/seq2seq/rnn_decoder.py
+++ encoder_decoder/seq2seq/rnn_decoder.py
@@ -95,7 +95,6 @@ class RNNDecoder(decoder.Decoder):
                                                         tf.reduce_max(projected_output, 1))
                             input = tf.cast(output_symbol, dtype=tf.int32)
                 input_embedding = tf.nn.embedding_lookup(self.embeddings(), input)
-
                 if self.use_attention:
                     output, state, attn_alignments = \
                         decoder_cell(input_embedding, state, attn_alignments)
