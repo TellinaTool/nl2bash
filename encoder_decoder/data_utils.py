39caa2799e0555f2b33538a22c0556bb39d80737
Victoria Lin <xilin@cs.washington.edu>
2017-04-14 15:53:04 -0700

    mf

--- encoder_decoder/data_utils.py
+++ encoder_decoder/data_utils.py
@@ -798,7 +798,8 @@ def group_data_by_nl(dataset, use_bucket=False, use_temp=True):
 
     grouped_dataset = {}
     for i in xrange(len(dataset)):
-        nl_str, cm_str, nl, cm, nl_full, cm_full = dataset[i]
+        print(dataset[i])
+        nl_str, cm_str, nl, cm, nl_full, cm_full, _ = dataset[i]
         if use_temp:
             words, _ = tokenizer.ner_tokenizer(nl_str)
             nl_template = " ".join(words)
