d966fbd30c7e34e9b6fb3022baf947fdcda2ceb4
Victoria Lin <xilin@cs.washington.edu>
2017-04-26 16:17:49 -0700

    mf

--- bashlex/normalizer.py
+++ bashlex/normalizer.py
@@ -1208,10 +1208,10 @@ def to_tokens(node, loose_constraints=False, ignore_flag_order=False,
                         token = '-{}'.format(node.arg_type)
                     else:
                         token = node.arg_type
-                    if au:
-                        token = unk_token
                 else:
                     token = node.arg_type
+                if au:
+                    token = unk_token
             else:
                 token = node.value
             if wat:
