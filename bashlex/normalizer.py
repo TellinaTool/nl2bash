c6553a130745426214ca66fbe45d871cfc7723c7
Victoria Lin <xilin@cs.washington.edu>
2017-04-27 00:11:15 -0700

    mf

--- bashlex/normalizer.py
+++ bashlex/normalizer.py
@@ -719,7 +719,7 @@ def normalize_ast(cmd, recover_quotes=True, verbose=False):
                         sub_command.children.append(repl_str_node2)
                         break
 
-    def normalize(node, current, arg_type=""):
+    def normalize(node, current, node_kind="", arg_type=""):
         # recursively normalize each subtree
         if not type(node) is bast.node:
             raise ValueError('type(node) is not ast.node')
@@ -1213,7 +1213,7 @@ def to_tokens(node, loose_constraints=False, ignore_flag_order=False,
             else:
                 token = node.value
             if au and node.is_open_vocab():
-                token = '__LF__' + unk_token
+                token = '__LF__' + token
             if wat:
                 token = token + "_" + node.arg_type
             if ia and node.to_index():
