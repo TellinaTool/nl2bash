6f235bce45a343f9cb7962a318d404592133d3aa
Victoria Lin <xilin@cs.washington.edu>
2017-04-29 13:44:37 -0700

    mf

--- encoder_decoder/decoder.py
+++ encoder_decoder/decoder.py
@@ -239,7 +239,8 @@ class AttentionCellWrapper(tf.nn.rnn_cell.RNNCell):
         self.attention_vars = True
         return ds, alignments
 
-    def __call__(self, input_embedding, state, attn_alignments=None, scope=None):
+    def __call__(self, input_embedding, state, attn_alignments=None,
+                 scope=None):
         if nest.is_sequence(state):
             dim = state[1].get_shape()[1].value
         else:
