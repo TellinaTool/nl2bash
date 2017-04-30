bdc1c2c1970c8f4f7f3962d54cfd969dc7e7da88
Victoria Lin <xilin@cs.washington.edu>
2017-04-30 12:31:52 -0700

    mf

--- encoder_decoder/data_utils.py
+++ encoder_decoder/data_utils.py
@@ -723,7 +723,7 @@ def prepare_bash(FLAGS, verbose=False):
     nl_vocab, rev_nl_vocab = initialize_vocabulary(nl_vocab_path)
     cm_vocab, rev_cm_vocab = initialize_vocabulary(cm_vocab_path)
 
-    generation_mask = np.zeros([FLAGS.target_vocab_size], dtype=np.float32)
+    generation_mask = np.zeros([FLAGS.tg_vocab_size], dtype=np.float32)
     if FLAGS.explain:
         for v in nl_vocab:
             if not v.startswith("__LF__"):
