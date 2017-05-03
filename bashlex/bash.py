f570ea0b1a5bd96a4faa9e006ca4929a038fc75d
Victoria Lin <xilin@cs.washington.edu>
2017-05-03 12:45:26 -0700

    generate evaluation sheet for low-frequency commands

--- bashlex/bash.py
+++ bashlex/bash.py
@@ -2,7 +2,7 @@
 Gazetteers for bash.
 """
 
-head_commands = [
+utilities = [
     "find",
     "xargs",
     "grep",
@@ -30,6 +30,28 @@ head_commands = [
     "cpio",
     "dirname",
     "tee",
+    "rename",
+    "rmdir",
+    "mkdir",
+    "less",
+    "md5sum",
+    "compress"
+]
+
+utilities_20_to_15 = [
+    "du",
+    "file",
+    "cat",
+    "basename",
+    "cut",
+    "uniq",
+    "pwd",
+    "cpio",
+    "dirname",
+    "tee"
+]
+
+utilities_15_to_10 = [
     "rename",
     "rmdir",
     "mkdir",
