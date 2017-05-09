7ac755db365300a2c8ee8abdac4b05d65a20be3e
Victoria Lin <xilin@cs.washington.edu>
2017-05-09 09:27:57 -0700

    update DropoutWrapper implementation

--- encoder_decoder/rnn.py
+++ encoder_decoder/rnn.py
@@ -419,10 +419,61 @@ class DropoutWrapper(tf.nn.rnn_cell.RNNCell):
 							 self._output_keep_prob)
 	return output, new_state
 
+
 def _enumerated_map_structure(map_fn, *args, **kwargs):
   ix = [0]
   def enumerated_fn(*inner_args, **inner_kwargs):
 	r = map_fn(ix[0], *inner_args, **inner_kwargs)
 	ix[0] += 1
 	return r
-  return nest.map_structure(enumerated_fn, *args, **kwargs)
+  return map_structure(enumerated_fn, *args, **kwargs)
+
+
+def map_structure(func, *structure, **check_types_dict):
+  """Applies `func` to each entry in `structure` and returns a new structure.
+  Applies `func(x[0], x[1], ...)` where x[i] is an entry in
+  `structure[i]`.  All structures in `structure` must have the same arity,
+  and the return value will contain the results in the same structure.
+  Args:
+    func: A callable that acceps as many arguments are there are structures.
+    *structure: scalar, or tuple or list of constructed scalars and/or other
+      tuples/lists, or scalars.  Note: numpy arrays are considered scalars.
+    **check_types_dict: only valid keyword argument is `check_types`. If set to
+      `True` (default) the types of iterables within the  structures have to be
+      same (e.g. `map_structure(func, [1], (1,))` raises a `TypeError`
+      exception). To allow this set this argument to `False`.
+  Returns:
+    A new structure with the same arity as `structure`, whose values correspond
+    to `func(x[0], x[1], ...)` where `x[i]` is a value in the corresponding
+    location in `structure[i]`. If there are different sequence types and
+    `check_types` is `False` the sequence types of the first structure will be
+    used.
+  Raises:
+    TypeError: If `func` is not callable or if the structures do not match
+      each other by depth tree.
+    ValueError: If no structure is provided or if the structures do not match
+      each other by type.
+    ValueError: If wrong keyword arguments are provided.
+  """
+  if not callable(func):
+    raise TypeError("func must be callable, got: %s" % func)
+
+  if not structure:
+    raise ValueError("Must provide at least one structure")
+
+  if check_types_dict:
+    if "check_types" not in check_types_dict or len(check_types_dict) > 1:
+      raise ValueError("Only valid keyword argument is check_types")
+    check_types = check_types_dict["check_types"]
+  else:
+    check_types = True
+
+  for other in structure[1:]:
+    nest.assert_same_structure(structure[0], other, check_types=check_types)
+
+  flat_structure = [nest.flatten(s) for s in structure]
+  entries = zip(*flat_structure)
+
+  return nest.pack_sequence_as(
+      structure[0], [func(*x) for x in entries])
+
