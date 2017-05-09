45a2905b502486883c073dc8cb1653f8c666eb35
Victoria Lin <xilin@cs.washington.edu>
2017-05-09 09:14:11 -0700

    mf

--- encoder_decoder/graph_utils.py
+++ encoder_decoder/graph_utils.py
@@ -232,7 +232,8 @@ def create_multilayer_cell(type, scope, dim, num_layers,
         assert(input_keep_prob >= 0 and output_keep_prob >= 0)
         if input_keep_prob < 1 or output_keep_prob < 1:
             cell = rnn.DropoutWrapper(cell, input_keep_prob=input_keep_prob,
-                output_keep_prob=output_keep_prob, variational_recurrent=False)
+                output_keep_prob=output_keep_prob, variational_recurrent=False,
+                dtype=tf.float32)
     return cell
 
 
