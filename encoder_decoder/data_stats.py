488dc3bdc60a7da119f74ccf58317830e3904477
Victoria Lin <xilin@cs.washington.edu>
2017-05-09 09:04:54 -0700

    add variational dropout on recurrent cells

--- encoder_decoder/data_stats.py
+++ encoder_decoder/data_stats.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import collections
 from encoder_decoder.data_utils import *
 
 def ratio(ll, x):
@@ -29,35 +28,3 @@ def entropy(data):
         p = float(c) / len(keys)
         ent += -p * np.log(p)
     return ent
-
-def data_stats(FLAGS):
-    train_set, dev_set, test_set = load_data(FLAGS)
-    train_by_nl = group_data_by_nl(train_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print(len(train_by_nl))
-    print("train cmd/nl ratio = {}".format(ratio(train_by_nl, 1)))
-    print("train %nl(cmd+) = {}".format(pp(train_by_nl)))
-    print("train entropy = {}".format(entropy(train_set)))
-    dev_by_nl = group_data_by_nl(dev_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print("dev cmd/nl ratio = {}".format(ratio(dev_by_nl, 1)))
-    print("dev %nl(cmd+) = {}".format(pp(dev_by_nl)))
-    print("dev nl overlap = {}".format(overlap(train_by_nl, dev_by_nl)))
-    print("dev entropy = {}".format(entropy(dev_set)))
-    test_by_nl = group_data_by_nl(test_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print("test cmd/nl ratio = {}".format(ratio(test_by_nl, 1)))
-    print("test %nl(cmd+) = {}".format(pp(test_by_nl)))
-    print("test nl overlap = {}".format(overlap(train_by_nl, test_by_nl)))
-    print("test entropy = {}".format(entropy(test_set)))
-    print("total entropy = {}".format(entropy(train_set + dev_set + test_set)))
-    train_by_cm = group_data_by_cm(train_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print(len(train_by_cm))
-    print("train nl/cmd ratio = {}".format(ratio(train_by_cm, 0)))
-    print("train %cmd(nl+) = {}".format(pp(train_by_cm)))
-    dev_by_cm = group_data_by_cm(dev_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print("dev nl/cmd ratio = {}".format(ratio(dev_by_cm, 0)))
-    print("dev %cmd(nl+) = {}".format(pp(dev_by_cm)))
-    print("dev cm overlap = {}".format(overlap(train_by_cm, dev_by_cm)))
-    test_by_cm = group_data_by_cm(test_set, use_temp=FLAGS.dataset.startswith("bash"))
-    print("test nl/cmd ratio = {}".format(ratio(test_by_cm, 0)))
-    print("test %cmd(nl+) = {}".format(pp(test_by_cm)))
-    print("test cm overlap = {}".format(overlap(train_by_cm, test_by_cm)))
-
