529f0af496333dbdda570ed39189de26c5fd6b47
Victoria Lin <xilin@cs.washington.edu>
2017-05-10 14:21:39 -0700

    clean up experiment scripts

---
+++ experiments/bash-run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Set up path to CUDA library
+source ~/.profile
+
+export PYTHONPATH=`pwd`'/..'
+
+ARGS=${@:1}
+
+python3 -m encoder_decoder.translate \
+    --nl_known_vocab_size 700 \
+    --cm_known_vocab_size 400 \
+    --nl_vocab_size 3100 \
+    --cm_vocab_size 3400 \
+    --encoder_topology birnn \
+    --batch_size 32 \
+    --token_decoding_algorithm beam_search \
+    --beam_size 100 \
+    --alpha 1.0 \
+    --num_nn_slot_filling 5 \
+    ${ARGS}
+
