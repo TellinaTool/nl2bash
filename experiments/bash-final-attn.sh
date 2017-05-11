0d2f4951744365ef814152853b055ef634822f19
Victoria Lin <xilin@cs.washington.edu>
2017-05-10 21:37:48 -0700

    mf

--- experiments/bash-final-attn.sh
+++ experiments/bash-final-attn.sh
@@ -4,4 +4,4 @@
 
 ARGS=${@:1}
 
-./run.sh --batch_size 16 --encoder_input_keep 0.5 --decoder_input_keep 0.5 --encoder_output_keep 0.5 --decoder_output_keep 0.5 --dataset bash.final --attention_input_keep 0.5 --attention_output_keep 0.5 --decoding_algorithm beam_search --beam_size 100 --alpha 1.0 --encoder_topology birnn --dim 400 --num_epochs 14 --steps_per_epoch 4000 --num_layers 1 --learning_rate 0.0001 --beta 0 --create_fresh_params --normalized --use_attention --num_nn_slot_filling 10 ${ARGS}
+./run.sh --batch_size 16 --sc_input_keep 0.5 --sc_input_keep 0.5 --sc_output_keep 0.5 --sc_output_keep 0.5 --dataset bash --attention_input_keep 1.0 --attention_output_keep 1.0 --token_decoding_algorithm beam_search --beam_size 100 --alpha 1.0 --encoder_topology birnn --sc_token_dim 200 --num_epochs 40 --steps_per_epoch 4000 --learning_rate 0.0001 --create_fresh_params --normalized --tg_token_use_attention --tg_token_attn_fun non-linear --sc_token --num_nn_slot_filling 30 ${ARGS}
