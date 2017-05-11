aa8d4637dc7d449ef9358536697c9d8dc05ac597
Victoria Lin <xilin@cs.washington.edu>
2017-05-11 00:08:34 -0700

    put attention dropout back

--- encoder_decoder/decoder.py
+++ encoder_decoder/decoder.py
@@ -168,7 +168,7 @@ class AttentionCellWrapper(tf.nn.rnn_cell.RNNCell):
         :param use_copy: Copy source tokens to the target.
         :param copy_fun: Parameterization of the copying function.
         """
-        # attention_states = tf.nn.dropout(attention_states, attention_input_keep)
+        attention_states = tf.nn.dropout(attention_states, attention_input_keep)
         attn_length = attention_states.get_shape()[1].value
         attn_dim = attention_states.get_shape()[2].value
 
@@ -286,10 +286,9 @@ class AttentionCellWrapper(tf.nn.rnn_cell.RNNCell):
 
         with tf.variable_scope("AttnOutputProjection"):
             # attention mechanism on output state
-            # output = tf.nn.rnn_cell._linear(
-            #     tf.nn.dropout(attn_state, self.attention_output_keep),
-            #     dim, True)
-            output = tf.nn.rnn_cell._linear(attn_state, dim, True)
+            output = tf.nn.rnn_cell._linear(
+                tf.nn.dropout(attn_state, self.attention_output_keep), dim,
+                True)
 
         self.attention_cell_vars = True
 
