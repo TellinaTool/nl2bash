38b49ffa96004779f7e449fcdd442e4f4aa0a643
Victoria Lin <xilin@cs.washington.edu>
2017-05-26 00:33:46 -0700

    mf

--- nlp_tools/ner.py
+++ nlp_tools/ner.py
@@ -100,8 +100,7 @@ def annotate(tokens):
 
     # -- File
     _FILE_RE = re.compile(constants.include_quotations(r'(([^"\']*\.[^ "\']+)|' +
-        r'(([^"\']*\/)+[^"\']*)|' + constants._FILE_EXTENSION_RE + '|' + 
-        constants._FILE_EXTENSION_RE.upper() + ')'))
+        r'(([^"\']*\/)+[^"\']*)|' + constants._FILE_EXTENSION_RE + ')'))
     sentence = annotate_ner(_FILE_RE, constants._FILE, sentence, entities)
     
     # -- Other patterns
