e349f37947b8a094eef5d632282c650508c610ae
Victoria Lin <xilin@cs.washington.edu>
2017-05-26 15:55:29 -0700

    update experiment scripts

--- experiments/bash-copy.sh
+++ experiments/bash-copy.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-# reproduce experiments using seq2seq with attention model on the bash dataset
+# reproduce experiments using CopyNet on the bash dataset
 
 ARGS=${@:1}
 
 ./bash-run.sh \
     --dataset bash \
-    --sc_token_dim 150 \
     --learning_rate 0.0001 \
+    --sc_token_dim 150 \
     --steps_per_epoch 4000 \
     --tg_token_use_attention \
     --tg_token_attn_fun non-linear \
     --universal_keep 0.6 \
-    --sc_input_keep 1.0 \
-    --tg_input_keep 1.0 \
-    --sc_output_keep 1.0 \
-    --tg_output_keep 1.0 \
+    --sc_input_keep 0.5 \
+    --tg_input_keep 0.5 \
+    --sc_output_keep 0.5 \
+    --tg_output_keep 0.5 \
     --attention_input_keep 1.0 \
     --attention_output_keep 1.0 \
     --beta 0 \
+    --use_copy \
+    --copy_fun explicit \
     --create_fresh_params \
     ${ARGS}
