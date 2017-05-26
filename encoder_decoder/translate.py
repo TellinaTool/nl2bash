0fde8006393f145af52f42682e42fbae156eff82
Victoria Lin <xilin@cs.washington.edu>
2017-05-26 15:54:42 -0700

    mf

--- encoder_decoder/translate.py
+++ encoder_decoder/translate.py
@@ -562,7 +562,7 @@ def main(_):
         if FLAGS.gen_eval_sheet:
             gen_eval_sheet(dataset)
         elif FLAGS.eval:
-            eval(dataset, verbose=True)
+            eval(dataset, verbose=False)
         elif FLAGS.manual_eval:
             manual_eval(dataset, 100)
         elif FLAGS.eval_slot_filling:
