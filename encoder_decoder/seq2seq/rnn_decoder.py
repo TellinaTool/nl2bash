64aff775dceda4939590db6fe2cff184d8781536
Victoria Lin <xilin@cs.washington.edu>
2017-05-31 17:00:50 -0700

    mf

--- encoder_decoder/seq2seq/rnn_decoder.py
+++ encoder_decoder/seq2seq/rnn_decoder.py
@@ -208,9 +208,14 @@ class RNNDecoder(decoder.Decoder):
                 #        [batch_size*self.beam_size, :, dim])
                 # GRU: [batch_size*self.beam_size, :, dim]
                 if self.rnn_cell == 'lstm':
-                    states = [zip(tf.split(1, c_states.get_shape()[1], c_states),
-                                 tf.split(1, h_states.get_shape()[1], h_states))
-                              for c_states, h_states in past_cell_states]
+                    if self.num_layers == 1:
+                        c_states, h_states = past_cell_states
+                        states = zip(tf.split(1, c_states.get_shape()[1], c_states),
+                                      tf.split(1, h_states.get_shape()[1], h_states))
+                    else:
+                        states = [zip(tf.split(1, c_states.get_shape()[1], c_states),
+                                      tf.split(1, h_states.get_shape()[1], h_states))
+                                  for c_states, h_states in past_cell_states]
                 elif self.rnn_cell in ['gru', 'ran']:
                     states = tf.split(1, past_cell_states.get_shape()[1],
                                   past_cell_states)[1:]
