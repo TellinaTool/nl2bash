d012bcad027bacd130a808a7fcaf5522eaeeb764
Victoria Lin <xilin@cs.washington.edu>
2017-06-05 20:29:50 -0700

    mf

--- encoder_decoder/decode_tools.py
+++ encoder_decoder/decode_tools.py
@@ -304,8 +304,8 @@ def decode(encoder_full_inputs, model_outputs, FLAGS, vocabs, sc_fillers=None,
                 output_example = True
             else:
                 # Step 3: match the fillers to the argument slots
+                batch_sc_fillers = sc_fillers[batch_id]
                 if len(tg_slots) >= len(batch_sc_fillers):
-                    batch_sc_fillers = sc_fillers[batch_id]
                     if FLAGS.use_copy and FLAGS.copy_fun == 'supervised':
                         target_ast, target, _ = slot_filling.stable_slot_filling(
                             output_tokens, batch_sc_fillers, tg_slots,
