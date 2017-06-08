356172ed3f37de57f3a1f5bd0fcb5b3e3da9bd02
Chenglong <mestway@gmail.com>
2017-06-08 13:56:02 -0700

    crawling file

--- grammar/extract_man.py
+++ grammar/extract_man.py
@@ -8,14 +8,10 @@ out_dir = "gnu_man_synopsis"
 def main():
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-
     top_cmds = map(lambda x: x.strip(), open(top_cmd_list_file,"r").readlines())
     for cmd in top_cmds:
-        try:
-            output = subprocess.check_output([cmd,'--help'])
-            print output
-        except:
-            continue
+        f = open(os.path.join(out_dir, cmd), "w")
+        out = subprocess.call([cmd, "--help"], stdout=f)
 
 if __name__ == '__main__':
     main()
