a790a2e43d3a35866d7414d52d1042566095718c
Victoria Lin <xilin@cs.washington.edu>
2017-06-10 00:40:18 -0700

    mf

--- nlp_tools/slot_filling.py
+++ nlp_tools/slot_filling.py
@@ -70,10 +70,11 @@ def stable_slot_filling(template_tokens, sc_fillers, tg_slots, pointer_targets,
                 ff = len(encoder_outputs) - f - 1
                 cm_slots_keys = list(tg_slots.keys())
                 for s in cm_slots_keys:
-                    X.append(np.expand_dims(np.concatenate(
-                        [encoder_outputs[ff:ff+1], decoder_outputs[s:s+1]], axis=1), 0))
+                    X.append(np.concatenate(
+                        [encoder_outputs[ff:ff+1], decoder_outputs[s:s+1]], axis=1))
                 X = np.concatenate(X, axis=0)
                 X = X / norm(X, axis=1)[:, None]
+                print(X.shape)
                 raw_scores = slot_filling_classifier.predict(X)
                 for ii in xrange(len(raw_scores)):
                     s = cm_slots_keys[ii]
