e932afa11eb41995e34956d95be09482392dac29
Victoria Lin <xilin@cs.washington.edu>
2017-08-15 17:55:16 -0700

    mf

--- encoder_decoder/decoder.py
+++ encoder_decoder/decoder.py
@@ -46,7 +46,7 @@ class Decoder(graph_utils.NNModel):
         self.decoding_algorithm = decoding_algorithm
 
         self.vocab_size = self.target_vocab_size
-        if self.use_copy:
+        if self.copynet:
             self.vocab_size += self.max_source_length
         
         # variable sharing
