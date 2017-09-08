2eff701191b3f6d0678d8e0f560d6b4cb596a243
Victoria Lin <xilin@cs.washington.edu>
2017-09-08 12:19:33 -0700

    mf

--- encoder_decoder/data_utils.py
+++ encoder_decoder/data_utils.py
@@ -20,7 +20,7 @@ if sys.version_info > (3, 0):
     from six.moves import xrange
 
 from bashlint import bash, nast, data_tools
-from nlp_tools import constants, ops, slot_filling, tokenizer
+from nlp_tools import constants, ops, tokenizer
 
 # Special token symbols
 _PAD = "__SP__PAD"
