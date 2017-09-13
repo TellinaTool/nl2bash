5104118ab301401cc08985efc6a3bccce4c33ab7
Victoria Lin <xilin@cs.washington.edu>
2017-09-12 23:45:06 -0700

    remove old experiment scripts

--- experiments/bash-final-attn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# script to reproduce seq2seq with attention model on the bash dataset
-
-ARGS=${@:1}
-
-./run.sh --batch_size 16 --sc_input_keep 0.5 --sc_input_keep 0.5 --sc_output_keep 0.5 --sc_output_keep 0.5 --dataset bash --attention_input_keep 1.0 --attention_output_keep 1.0 --token_decoding_algorithm beam_search --beam_size 100 --alpha 1.0 --encoder_topology birnn --sc_token_dim 200 --num_epochs 40 --steps_per_epoch 4000 --learning_rate 0.0001 --create_fresh_params --normalized --tg_token_use_attention --tg_token_attn_fun non-linear --sc_token --num_nn_slot_filling 30 ${ARGS}
