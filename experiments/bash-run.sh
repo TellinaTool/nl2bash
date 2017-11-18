1fafaadac206a29d3f10f8e52790b8b56769d9d9
todpole3 <xilin@cs.washington.edu>
2017-11-18 06:16:34 +0000

    update num_epochs

--- experiments/bash-run.sh
+++ experiments/bash-run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set up path to CUDA library
 source ~/.profile
@@ -10,7 +10,7 @@ ARGS=${@:1}
 python3 -m encoder_decoder.translate \
     --rnn_cell gru \
     --encoder_topology birnn \
-    --num_epochs 5 \
+    --num_epochs 100 \
     --num_samples 256 \
     --variational_recurrent_dropout \
     --token_decoding_algorithm beam_search \
