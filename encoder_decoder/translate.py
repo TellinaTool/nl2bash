d6fc26b0ad40fb883eeb48d96390a7ae1f1f2024
Victoria Lin <xilin@cs.washington.edu>
2018-02-27 01:26:41 -0800

    minor fixes

--- encoder_decoder/translate.py
+++ encoder_decoder/translate.py
@@ -313,7 +313,7 @@ def main(_):
         elif FLAGS.gen_manual_evaluation_table:
             eval_tools.gen_evaluation_table(dataset, FLAGS)
         elif FLAGS.tabulate_example_predictions:
-            error_analysis.tabulate_example_predictions(dataset, FLAGS, num_examples=20)
+            error_analysis.tabulate_example_predictions(dataset, FLAGS, num_examples=100)
 
         elif FLAGS.gen_slot_filling_training_data:
             gen_slot_filling_training_data(FLAGS, [train_set, dev_set, test_set])
